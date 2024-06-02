7bc82062a3367a4207d249bd0933efda7b8827ac
Mega02600 <haouasjeremy@gmail.com>
2024-06-02 17:54:21 +0200

    correction des effets du zombie

--- src/main/java/fr/nicknqck/roles/mc/overworld/Zombie.java
+++ src/main/java/fr/nicknqck/roles/mc/overworld/Zombie.java
@@ -83,10 +83,10 @@ public class Zombie extends RoleBase {
                                         if (gameState.nightTime){
                                             addBonusResi(10);
                                         } else {
-                                            givePotionEffet(PotionEffectType.INCREASE_DAMAGE, 2, 1, true);
+                                            givePotionEffet(PotionEffectType.INCREASE_DAMAGE, 2*20, 1, true);
                                         }
                                     } else {
-                                        givePotionEffet(PotionEffectType.SLOW, 2, 1, true);
+                                        givePotionEffet(PotionEffectType.SLOW, 2*20, 1, true);
                                     }
                                 } else {
                                     owner.sendMessage("Vous perdez votre "+AllDesc.slow+" §41 §ret vous pouvez manger encore §c"+nmbCerveau+" Cerveau");
