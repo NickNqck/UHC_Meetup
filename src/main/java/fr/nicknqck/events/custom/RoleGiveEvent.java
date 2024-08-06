62c27570ade0208446c47d1d17e72e5daa326879
NickNqck <jadorevolvic@gmail.com>
2024-08-06 17:40:43 +0200

    Modification suite à la suppression du CustomEventBase

--- src/main/java/fr/nicknqck/events/custom/RoleGiveEvent.java
+++ src/main/java/fr/nicknqck/events/custom/RoleGiveEvent.java
@@ -4,19 +4,29 @@ import fr.nicknqck.GameState;
 import fr.nicknqck.player.GamePlayer;
 import fr.nicknqck.roles.builder.RoleBase;
 import lombok.Getter;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 
 @Getter
-public class RoleGiveEvent extends CustomEventBase {
+public class RoleGiveEvent extends Event {
 
     private final GameState gameState;
     private final RoleBase role;
     private final GameState.Roles type;
     private final GamePlayer gamePlayer;
+    private static final HandlerList handlers = new HandlerList();
     public RoleGiveEvent(GameState gameState, RoleBase role, GameState.Roles roleType, GamePlayer gamePlayer) {
         super();
         this.gameState = gameState;
         this.role = role;
         this.type = roleType;
         this.gamePlayer = gamePlayer;
+    }
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
     }
 }
