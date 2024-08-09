110df2610d5ff20e522a956f586c312c2efe83fb
Jérémy <haouasjeremy@gmail.com>
2024-08-09 23:19:48 +0200

    Création de la classe Araignée... Squelette terminé

---
+++ src/main/java/fr/nicknqck/roles/mc/overworld/AraigneeVenimeuse.java
@@ -0,0 +1,69 @@
+package fr.nicknqck.roles.mc.overworld;
+
+import fr.nicknqck.GameState;
+import fr.nicknqck.roles.builder.RoleBase;
+import fr.nicknqck.roles.builder.TeamList;
+import fr.nicknqck.utils.itembuilder.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
+
+import java.util.UUID;
+
+public class AraigneeVenimeuse extends RoleBase {
+
+    private final ItemStack ToileItem = new ItemBuilder(Material.WEB).setName("§aToile d'araignée").setLore("§7Vous permez de poser une toile d'araigée sous un joueur").addEnchant(Enchantment.PROTECTION_ENVIRONMENTAL, 1).hideAllAttributes().toItemStack();
+    private boolean poison = false;
+
+    public AraigneeVenimeuse(UUID player) {
+        super(player);
+    }
+
+    @Override
+    public String[] Desc() {
+        return new String[0];
+    }
+
+    @Override
+    public ItemStack[] getItems() {
+        return new ItemStack[]{
+            ToileItem,
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public GameState.Roles getRoles() {
+        return null;
+    }
+
+    @Override
+    public TeamList getOriginTeam() {
+        return null;
+    }
+
+    @Override
+    public void resetCooldown() {
+
+    }
+
+    @Override
+    public void onALLPlayerDamageByEntity(EntityDamageByEntityEvent event, Player victim, Entity entity) {
+        if (entity.getUniqueId() == owner.getUniqueId()){
+            if(owner.getItemInHand()){
+                if (poison){
+                    givePotionEffet(victim, PotionEffectType.POISON, 2,1,true);
+                }
+            }
+        }
+        super.onALLPlayerDamageByEntity(event, victim, entity);
+    }
+}
--- src/main/java/fr/nicknqck/roles/mc/overworld/Squelette.java
+++ src/main/java/fr/nicknqck/roles/mc/overworld/Squelette.java
@@ -5,6 +5,8 @@ import fr.nicknqck.roles.desc.AllDesc;
 import fr.nicknqck.roles.mc.builders.OverWorldRoles;
 import fr.nicknqck.utils.itembuilder.ItemBuilder;
 import fr.nicknqck.utils.Loc;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
@@ -21,6 +23,7 @@ public class Squelette extends OverWorldRoles {
     private final ItemStack BontoutouItem = new ItemBuilder(Material.BONE).addEnchant(Enchantment.ARROW_DAMAGE, 1).hideAllAttributes().setName("§aBon TouTou").setLore("§7Vous permez d'apprivoiser un loup").toItemStack();
     private int KorosuCount = 0;
     private boolean ZombieSound = false;
+
     public Squelette(UUID player) {
         super(player);
     }
@@ -29,12 +32,12 @@ public class Squelette extends OverWorldRoles {
     public String[] Desc() {
         return new String[]{
                 AllDesc.bar,
-                AllDesc.role+"§aSquelette",
-                AllDesc.objectifteam+"§aOverWorld",
+                AllDesc.role + "§aSquelette",
+                AllDesc.objectifteam + "§aOverWorld",
                 "",
                 AllDesc.items,
                 "",
-                AllDesc.point+"§aKorosu shi no yumi :§r Un arc §1power 4",
+                AllDesc.point + "§aKorosu shi no yumi :§r Un arc §1power 4",
                 "",
                 AllDesc.particularite,
                 "",
@@ -45,6 +48,7 @@ public class Squelette extends OverWorldRoles {
 
         };
     }
+
     @Override
     public GameState.Roles getRoles() {
         return GameState.Roles.Squelette;
@@ -70,23 +74,33 @@ public class Squelette extends OverWorldRoles {
 
     @Override
     public void PlayerKilled(Player killer, Player victim, GameState gameState) {
-        if (gameState.getInGamePlayers().contains(killer)){
-            if (victim.getUniqueId() == owner.getUniqueId()){
+        if (gameState.getInGamePlayers().contains(killer)) {
+            if (victim.getUniqueId() == owner.getUniqueId()) {
                 giveItem(killer, false, BontoutouItem);
-                killer.sendMessage("Vous venez de tuez §a"+owner.getName()+" §rqui était §aSquelette §r,vous obtenez donc un os §aBon TouTou §rqui vous permez d'apprivoiser un loup");
+                killer.sendMessage("Vous venez de tuez §a" + owner.getName() + " §rqui était §aSquelette §r,vous obtenez donc un os §aBon TouTou §rqui vous permez d'apprivoiser un loup");
             }
         }
         super.PlayerKilled(killer, victim, gameState);
     }
 
 
-
     @Override
     public void onProjectileLaunch(Projectile entity, Player shooter) {
-        if (shooter.getUniqueId().equals(owner.getUniqueId())){
-            if (entity instanceof Arrow){
-                if (KorosuCount == 0){
-                    KorosuCount++;
+        if (shooter.getUniqueId().equals(owner.getUniqueId())) {
+            if (entity instanceof Arrow) {
+                if (KorosuCount == 0) KorosuCount++;
+                else {
+                    Player target = null;
+                    for (Player onlinePlayer : shooter.getWorld().getPlayers()) {
+                        if (!onlinePlayer.getGameMode().equals(GameMode.SURVIVAL)) continue;
+                        if (target == null) target = onlinePlayer;
+                        double distance = shooter.getLocation().distance(target.getLocation());
+                        if (distance > shooter.getLocation().distance(onlinePlayer.getLocation()))
+                            target = onlinePlayer;
+                    }
+                    if(target == null) return;
+                    entity.setVelocity(target.getLocation().toVector().subtract(entity.getLocation().toVector()));
+                    KorosuCount = 0;
                 }
             }
         }
@@ -95,12 +109,12 @@ public class Squelette extends OverWorldRoles {
 
     @Override
     public void onAllPlayerMoove(PlayerMoveEvent e, Player moover) {
-        if (moover.getUniqueId() == owner.getUniqueId()){
+        if (moover.getUniqueId() == owner.getUniqueId()) {
             if (!ZombieSound) {
                 for (Player p : Loc.getNearbyPlayersExcept(owner, 15)) {
                     if (getPlayerRoles(p) instanceof Zombie) {
-                    playSound(owner, "entity.zombie.ambient");
-                    ZombieSound = true;
+                        playSound(owner, "entity.zombie.ambient");
+                        ZombieSound = true;
                     }
                 }
             }
