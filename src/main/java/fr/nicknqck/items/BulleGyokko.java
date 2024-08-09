bedf1ee47ea61d69c4a62e8265a2acc76767777e
NickNqck <jadorevolvic@gmail.com>
2024-08-10 00:05:21 +0200

    Optimisation BulleGyokko

--- src/main/java/fr/nicknqck/items/BulleGyokko.java
+++ src/main/java/fr/nicknqck/items/BulleGyokko.java
@@ -91,12 +91,8 @@ public class BulleGyokko implements Listener{
 							water.put(waters, waters.getType());
 							waters.setType(Material.WATER);
 						}
-						Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> {
-							iron.keySet().forEach(ez -> ez.setType(iron.get(ez)));
-		                }, 20*60);
-						Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> {
-							water.keySet().forEach(ez -> ez.setType(water.get(ez)));
-		                }, 1190);//1190 = 20*59+10
+						Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> iron.keySet().forEach(ez -> ez.setType(iron.get(ez))), 20*60);
+						Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> water.keySet().forEach(ez -> ez.setType(water.get(ez))), 1190);//1190 = 20*59+10
                         gyokko.bullecooldown = 60*7;
 					}else {
 						gyokko.sendCooldown(gyokko.owner, gyokko.bullecooldown);
@@ -106,74 +102,4 @@ public class BulleGyokko implements Listener{
 			}
 		}
 	}
-	
-	public Set<Location> sphere(Location location, int radius, boolean hollow){
-	       Set<Location> blocks = new HashSet<Location>();
-	        World world = location.getWorld();
-	        int X = location.getBlockX();
-	        int Y = location.getBlockY();
-	        int Z = location.getBlockZ();
-	        int radiusSquared = radius * radius;
-
-	        if(hollow){
-	            for (int x = X - radius; x <= X + radius; x++) {
-	                for (int y = Y - radius; y <= Y + radius; y++) {
-	                    for (int z = Z - radius; z <= Z + radius; z++) {
-	                        if ((X - x) * (X - x) + (Y - y) * (Y - y) + (Z - z) * (Z - z) <= radiusSquared) {
-	                            Location block = new Location(world, x, y, z);
-	                            blocks.add(block);
-	                        }
-	                    }
-	                }
-	            }
-	            return makeHollow(blocks, true);
-	        } else {
-	            for (int x = X - radius; x <= X + radius; x++) {
-	                for (int y = Y - radius; y <= Y + radius; y++) {
-	                    for (int z = Z - radius; z <= Z + radius; z++) {
-	                        if ((X - x) * (X - x) + (Y - y) * (Y - y) + (Z - z) * (Z - z) <= radiusSquared) {
-	                            Location block = new Location(world, x, y, z);
-	                            blocks.add(block);
-	                        }
-	                    }
-	                }
-	            }
-	            return blocks;
-	        }
-	    }
-	private Set<Location> makeHollow(Set<Location> blocks, boolean sphere){
-	        Set<Location> edge = new HashSet<Location>();
-	        if(!sphere){
-	            for(Location l : blocks){
-	                World w = l.getWorld();
-	                int X = l.getBlockX();
-	                int Y = l.getBlockY();
-	                int Z = l.getBlockZ();
-	                Location front = new Location(w, X + 1, Y, Z);
-	                Location back = new Location(w, X - 1, Y, Z);
-	                Location left = new Location(w, X, Y, Z + 1);
-	                Location right = new Location(w, X, Y, Z - 1);
-	                if(!(blocks.contains(front) && blocks.contains(back) && blocks.contains(left) && blocks.contains(right))){
-	                    edge.add(l);
-	                }
-	            }
-	        } else {
-	            for(Location l : blocks){
-	                World w = l.getWorld();
-	                int X = l.getBlockX();
-	                int Y = l.getBlockY();
-	                int Z = l.getBlockZ();
-	                Location front = new Location(w, X + 1, Y, Z);
-	                Location back = new Location(w, X - 1, Y, Z);
-	                Location left = new Location(w, X, Y, Z + 1);
-	                Location right = new Location(w, X, Y, Z - 1);
-	                Location top = new Location(w, X, Y + 1, Z);
-	                Location bottom = new Location(w, X, Y - 1, Z);
-	                if(!(blocks.contains(front) && blocks.contains(back) && blocks.contains(left) && blocks.contains(right) && blocks.contains(top) && blocks.contains(bottom))){
-	                    edge.add(l);
-	                }
-	            }
-	        }
-	        return edge;
-	    }
 }
