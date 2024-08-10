fadc591beeb5c2d49cd8261a757279471ed424a1
NickNqck <jadorevolvic@gmail.com>
2024-08-10 22:59:03 +0200

    Buff de Kabuto en cours

--- src/main/java/fr/nicknqck/roles/ns/orochimaru/Kabuto.java
+++ src/main/java/fr/nicknqck/roles/ns/orochimaru/Kabuto.java
@@ -4,40 +4,81 @@ import fr.nicknqck.GameState;
 import fr.nicknqck.GameState.Roles;
 import fr.nicknqck.HubListener;
 import fr.nicknqck.Main;
+import fr.nicknqck.events.custom.EndGameEvent;
+import fr.nicknqck.events.custom.UHCDeathEvent;
 import fr.nicknqck.items.GUIItems;
+import fr.nicknqck.roles.builder.AutomaticDesc;
+import fr.nicknqck.roles.builder.EffectWhen;
 import fr.nicknqck.roles.builder.RoleBase;
 import fr.nicknqck.roles.desc.AllDesc;
 import fr.nicknqck.roles.ns.Chakras;
 import fr.nicknqck.roles.ns.Intelligence;
 import fr.nicknqck.roles.ns.builders.OrochimaruRoles;
 import fr.nicknqck.utils.GlobalUtils;
+import fr.nicknqck.utils.TripleMap;
 import fr.nicknqck.utils.itembuilder.ItemBuilder;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashMap;
 import java.util.UUID;
 
-public class Kabuto extends OrochimaruRoles {
+public class Kabuto extends OrochimaruRoles implements Listener {
+	private TextComponent desc;
+	private int ninjutsuCD = 0;
+	private final HashMap<Player, RoleBase> edoTensei = new HashMap<>();
+	private final HashMap<Player, Location> killLoc = new HashMap<>();
+	private boolean mortOrochimaru = false;
+	private boolean karinDeath = false;
 
 	public Kabuto(UUID player) {
 		super(player);
+	}
+
+	@Override
+	public void RoleGiven(GameState gameState) {
 		setChakraType(Chakras.SUITON);
-		Bukkit.getScheduler().scheduleSyncDelayedTask(Main.getPlugin(Main.class), () -> {
+		Bukkit.getScheduler().runTaskLaterAsynchronously(Main.getPlugin(Main.class), () -> {
 			if (!gameState.attributedRole.contains(Roles.Orochimaru)) {
 				onOrochimaruDeath(false);
-				owner.sendMessage("§5Orochimaru§7 n'étant pas dans la partie vous avez tout de même reçus les bonus dû à sa mort !");
+				owner.sendMessage("§5Orochimaru§7 n'étant pas dans la partie vous avez tout de même reçus les bonus dû à sa mort ! (§6/ns me§7)");
 			}
 		}, 20*5);
+		givePotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0, false, false), EffectWhen.PERMANENT);
+		getKnowedRoles().add(Orochimaru.class);
+		AutomaticDesc automaticDesc = new AutomaticDesc(this);
+		automaticDesc.addEffects(getEffects());
+		automaticDesc.setItems(new TripleMap<>(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{new TextComponent("§7Soigne quelqu'un en fonction du clique:\n\n" +
+				"§7     → Clique droit: En visant un joueur, celà permet de le§d soigner§7 de§c 2"+AllDesc.coeur+"\n\n" +
+				"§7     → Clique gauche: Vous§d soigne§7 de§c 2"+AllDesc.coeur)}), "§aNinjutsu Médical", 60*3));
+		automaticDesc.addParticularites(
+				new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{new TextComponent("§7A la mort de §5Karin§7 votre§a Ninjutsu Médical§d soignera§7 de§c 6"+AllDesc.coeur)}),
+				new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{new TextComponent("§7A la mort de§5 Jugo§7 vous obtiendrez un§a dash§7 qui vous propulsera§c 10blocs§7 en avant et infligera§c 2"+AllDesc.coeur+"§7 aux joueurs proche.")})
+		);
+		this.desc = automaticDesc.getText();
+		Bukkit.getPluginManager().registerEvents(this, Main.getInstance());
+		Bukkit.getScheduler().runTaskLaterAsynchronously(Main.getInstance(), () -> {
+			if (!gameState.attributedRole.contains(Roles.Karin)) {
+				onKarinDeath(false);
+			}
+		}, 20*10);
+		super.RoleGiven(gameState);
 	}
 	@Override
 	public Roles getRoles() {
@@ -51,8 +92,8 @@ public class Kabuto extends OrochimaruRoles {
 	public ItemStack[] getItems() {
 		if (mortOrochimaru) {
 			return new ItemStack[] {
-					EdoTenseiItem(),
-					NinjutsuMedicalItem()
+					NinjutsuMedicalItem(),
+					EdoTenseiItem()
 			};
 		}
 		return new ItemStack[] {
@@ -61,8 +102,7 @@ public class Kabuto extends OrochimaruRoles {
 	}
 	@Override
 	public String[] Desc() {
-		KnowRole(owner, Roles.Orochimaru, 5);
-		return new String[] {
+		return new String[] {/*
 				AllDesc.bar,
 				AllDesc.role+"§5Kabuto",
 				AllDesc.objectifteam+"§5Orochimaru",
@@ -82,16 +122,19 @@ public class Kabuto extends OrochimaruRoles {
 				"",
 				"A la mort de§5 Orochimaru§f vous obtenez l'item§5 Edo Tensei§f qui permet de tué une personne que vous avez précédemment tué",
 				"",
-				AllDesc.bar
+				AllDesc.bar*/
 		};
 	}
 	@Override
+	public TextComponent getComponent() {
+		return desc;
+	}
+	@Override
 	public void Update(GameState gameState) {
-		givePotionEffet(PotionEffectType.SPEED, Integer.MAX_VALUE, 1, false);
 		if (ninjutsuCD >= 0) {
 			ninjutsuCD--;
 			if (ninjutsuCD == 0) {
-				owner.sendMessage("§7Vous pouvez à nouveau soigner quelqu'un");
+				owner.sendMessage("§7Vous pouvez à nouveau§d soigner§7 quelqu'un.");
 			}
 		}
 	}
@@ -106,8 +149,6 @@ public class Kabuto extends OrochimaruRoles {
 	private ItemStack NinjutsuMedicalItem() {
 		return new ItemBuilder(Material.NETHER_STAR).setName("§aNinjutsu Médical").setLore("§7Permet de vous soignez vous ou un autre joueur").toItemStack();
 	}
-	private int ninjutsuCD = 0;
-	private final HashMap<Player, RoleBase> edoTensei = new HashMap<>();
 	@Override
 	public void OnAPlayerDie(Player player, GameState gameState, Entity killer) {
 		if (edoTensei.containsKey(player)) {
@@ -136,8 +177,6 @@ public class Kabuto extends OrochimaruRoles {
 			owner.sendMessage("§7C'est terrible !§5 Orochimaru§7 est§c mort§7, en son homage vous récupérez sa plus puissante technique, l'§5Edo Tensei§7.");
 		}
 	}
-	private final HashMap<Player, Location> killLoc = new HashMap<>();
-	private boolean mortOrochimaru = false;
 	@Override
 	public void onALLPlayerInteract(PlayerInteractEvent event, Player player) {
 		if (event.getItem() != null) {
@@ -155,14 +194,14 @@ public class Kabuto extends OrochimaruRoles {
 							ninjutsuCD = 60;
 						}
 						player.sendMessage("§dSoins !");
-						Heal(owner, 4);
+						Heal(owner, this.karinDeath ? 10 : 4);
 					} else {
 						Player target = getTargetPlayer(owner, 30);
 						if (target == null) {
 							owner.sendMessage("§cIl faut viser un joueur !");
 							return;
 						}
-						Heal(target, 4);
+						Heal(target, this.karinDeath ? 10 : 4);
 						player.sendMessage("§dSoins !");
 						target.sendMessage("§5Kabuto§f vous à§d soigner§f !");
 						if (!mortOrochimaru) {
@@ -175,12 +214,10 @@ public class Kabuto extends OrochimaruRoles {
 			}
 		}
 	}
-
 	@Override
 	public Intelligence getIntelligence() {
 		return Intelligence.INTELLIGENT;
 	}
-
 	@Override
 	public boolean ItemUse(ItemStack item, GameState gameState) {
 		if (mortOrochimaru) {
@@ -249,9 +286,29 @@ public class Kabuto extends OrochimaruRoles {
 			}
 		}
 	}
-
 	@Override
 	public String getName() {
 		return "Kabuto";
+	}
+	@EventHandler
+	private void onEndGame(EndGameEvent event) {
+		HandlerList.unregisterAll(this);
+	}
+	@EventHandler
+	private void onUHCDeath(UHCDeathEvent event) {
+		if (event.getRole() != null) {
+			if (event.getRole() instanceof Karin && !this.karinDeath) {
+				onKarinDeath(true);
+			}
+		}
+	}
+	private void onKarinDeath(boolean msg) {
+		this.karinDeath = true;
+		if (msg) {
+			Player owner = Bukkit.getPlayer(getPlayer());
+			if (owner != null) {
+				owner.sendMessage("§5Karin§7 est malheuresement morte, celà vous a donnez des idées pour votre§a Ninjutsu Médical§7 il§d soignera§7 à partir de maintenant§c 5"+AllDesc.coeur+"§7 au lieu de§c 2§7.");
+			}
+		}
 	}
 }
