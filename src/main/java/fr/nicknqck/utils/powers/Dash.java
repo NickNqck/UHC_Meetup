f6b9f70ec6688f0c109fe79d4526536cc9ed3a6c
NickNqck <jadorevolvic@gmail.com>
2024-08-11 00:00:36 +0200

    Opti Dash

--- src/main/java/fr/nicknqck/utils/powers/Dash.java
+++ src/main/java/fr/nicknqck/utils/powers/Dash.java
@@ -11,13 +11,20 @@ import org.bukkit.util.Vector;
 import fr.nicknqck.Main;
 import fr.nicknqck.roles.desc.AllDesc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Dash {
 	private final Player target;
 	private int distance;
 	private boolean degat;
 	private double degats;
+	private final List<UUID> dashedPlayers;
+	private boolean isBreak;
 	public Dash(Player target) {
 		this.target = target;
+		dashedPlayers = new ArrayList<>();
 	}
 	public Dash start(int DegatDistance) {
 		new BukkitRunnable() {
@@ -30,12 +37,15 @@ public class Dash {
 					if (degat) {
 						for (Player p : Loc.getNearbyPlayersExcept(target, DegatDistance)) {
 							if (p.getUniqueId() != target.getUniqueId()) {
-								if (p.getHealth() - degats < 0) {
-									p.setHealth(1.0);
-								} else {
-									p.setHealth(p.getHealth()-degats);
+								if (!dashedPlayers.contains(p.getUniqueId())) {
+									if (p.getHealth() - degats < 0) {
+										p.setHealth(1.0);
+									} else {
+										p.setHealth(p.getHealth()-degats);
+									}
+									p.sendMessage("§7Vous avez subit§c "+degats/2+AllDesc.coeur+"§7 dû à un Dash");
+									dashedPlayers.add(p.getUniqueId());
 								}
-								p.sendMessage("§7Vous avez subit§c "+degats/2+AllDesc.coeur+"§7 dû à un Dash");
 							}
 						}
 					}
@@ -87,6 +97,10 @@ public class Dash {
 		this.distance = distance;
 		return this;
 	}
+	public Dash setBreak(boolean b) {
+		this.isBreak =b;
+		return this;
+	}
 	private void teleportOneBlockForward(Player player) {
         Location playerLocation = player.getLocation();
         Vector playerDirection = playerLocation.getDirection();
@@ -96,8 +110,29 @@ public class Dash {
         newLocation.setYaw(playerLocation.getYaw());
         newLocation.setPitch(playerLocation.getPitch());
         Block block = newLocation.getBlock();
+		if (isBreak) {
+			for (Block b : getSurroundingBlocks(player)) {
+				b.setType(Material.AIR);
+			}
+		}
         if (block.getType() == Material.AIR || block.getType().name().contains("WATER")) {
         	player.teleport(newLocation);
         }
     }
+	public List<Block> getSurroundingBlocks(Player player) {
+		List<Block> blocks = new ArrayList<>();
+		Location playerLocation = player.getLocation();
+
+		for (int x = -1; x <= 1; x++) {
+			for (int y = -1; y <= 1; y++) {
+				for (int z = -1; z <= 1; z++) {
+					Block block = playerLocation.clone().add(x, y, z).getBlock();
+					blocks.add(block);
+				}
+			}
+		}
+
+		return blocks;
+	}
+
 }
