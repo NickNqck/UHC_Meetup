fb89f04d2d55e80fdfefbff4999c40f0ef0dcfb2
NickNqck <jadorevolvic@gmail.com>
2024-08-10 17:39:20 +0200

    removed useless //

--- src/main/java/fr/nicknqck/player/GamePlayer.java
+++ src/main/java/fr/nicknqck/player/GamePlayer.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 @Getter
 public class GamePlayer {
-//class pour l'instant inutile mais permettra de faire qu'on puisse déco reco en game, (si tu vois sa @ moi je veux savoir ce que tu en pense
+
 	private final UUID uuid;
 	@Setter
 	private boolean isAlive;
