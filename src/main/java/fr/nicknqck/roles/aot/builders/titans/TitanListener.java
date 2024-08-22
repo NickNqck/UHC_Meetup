1618b1afcaf94ad328f02cab8bd75cda113a8efe
NickNqck <jadorevolvic@gmail.com>
2024-08-22 09:56:39 +0200

    pour la beauté du code

--- src/main/java/fr/nicknqck/roles/aot/builders/titans/TitanListener.java
+++ src/main/java/fr/nicknqck/roles/aot/builders/titans/TitanListener.java
@@ -73,104 +73,104 @@ public class TitanListener implements Listener{
 	}
 	
 	public void onSecond() {
-			if (getColossalCooldown() == 60*5) {
-				Titans.Colossal.getTitan().Transfo();
+		if (getColossalCooldown() == 60*5) {
+			Titans.Colossal.getTitan().Transfo();
+		}
+		if (getColossalCooldown() > 0) {
+			setColossalCooldown(getColossalCooldown()-1);
+		}
+		if (getColossalCooldown() == 0) {
+			if (getColossal() != null) {
+				Player player = Bukkit.getPlayer(getColossal());
+				if (player == null)return;
+				player.sendMessage("§7Vous pouvez à nouveau vous transformé en Titan "+Titans.Colossal.getTitan().getName());
+				setColossalCooldown(-5);
 			}
-			if (getColossalCooldown() > 0) {
-				setColossalCooldown(getColossalCooldown()-1);
-			}
-			if (getColossalCooldown() == 0) {
-				if (getColossal() != null) {
-					Player player = Bukkit.getPlayer(getColossal());
-					if (player == null)return;
-					player.sendMessage("§7Vous pouvez à nouveau vous transformé en Titan "+Titans.Colossal.getTitan().getName());
-					setColossalCooldown(-5);
+		}
+		if (getCuirasseCooldown() >0) {
+			setCuirasseCooldown(getCuirasseCooldown()-1);
+		}
+		if (getCuirasseCooldown() == 60*4) {
+			Titans.Cuirasse.getTitan().Transfo();
+		}
+		if (getCuirasseCooldown() == 0) {
+			if (getCuirasse() != null) {
+				Player player = Bukkit.getPlayer(getCuirasse());
+				if (player != null) {
+					player.sendMessage("§7Vous pouvez à nouveau vous transformé en Titan "+Titans.Cuirasse.getTitan().getName());
+					setCuirasseCooldown(-5);
 				}
 			}
-			if (getCuirasseCooldown() >0) {
-				setCuirasseCooldown(getCuirasseCooldown()-1);
+		}
+		if (getWarHammerCooldown()== 60*4) {
+			Titans.WarHammer.getTitan().Transfo();
+		}
+		if (getWarHammerCooldown() > 0) {
+			setWarHammerCooldown(getWarHammerCooldown()-1);
+		}
+		if (getWarHammerCooldown() == 0) {
+			if (getWarHammer() != null) {
+				Player player = Bukkit.getPlayer(getWarHammer());
+				if (player != null) {
+					player.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.WarHammer.getTitan().getName());
+				}
+				setWarHammerCooldown(-5);
 			}
-			if (getCuirasseCooldown() == 60*4) {
-				Titans.Cuirasse.getTitan().Transfo();
+		}
+		if (getMachoireCooldown() == 0) {
+			if (getMachoire() != null) {
+				Player p = Bukkit.getPlayer(getMachoire());
+				if (p != null) {
+					p.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.Machoire.getTitan().getName());
+				}
+				setMachoireCooldown(-5);
 			}
-			if (getCuirasseCooldown() == 0) {
-				if (getCuirasse() != null) {
-					Player player = Bukkit.getPlayer(getCuirasse());
-					if (player != null) {
-						player.sendMessage("§7Vous pouvez à nouveau vous transformé en Titan "+Titans.Cuirasse.getTitan().getName());
-						setCuirasseCooldown(-5);
-					}
+		}
+		if (getMachoireCooldown()>0) {
+			setMachoireCooldown(getMachoireCooldown()-1);
+		}
+		if (getMachoireCooldown() == 60*3) {
+			Titans.Machoire.getTitan().Transfo();
+		}
+		if (getAssaillantCooldown() > 0) {
+			setAssaillantCooldown(getAssaillantCooldown()-1);
+			if (getAssaillantCooldown() == 60*4) {
+				Titans.Assaillant.getTitan().Transfo();
+			}
+		}
+		if (getAssaillantCooldown() == 0) {
+			if (getAssaillant() != null) {
+				Player p = Bukkit.getPlayer(getAssaillant());
+				if (p != null) {
+					p.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.Assaillant.getTitan().getName());
+				}
+				setAssaillantCooldown(-5);
+			}
+		}
+		if (getBestialCooldown() > 0) {
+			setBestialCooldown(getBestialCooldown()-1);
+			if (getBestialCooldown() == 60*4) {
+				Titans.Bestial.getTitan().Transfo();
+			}
+		}
+		if (getBestialCooldown() == 0) {
+			if (getBestial() != null) {
+				Player p = Bukkit.getPlayer(getBestial());
+				if (p != null) {
+					p.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.Bestial.getTitan().getName());
+				}
+				setBestialCooldown(-5);
+			}
+		}
+		for (Titans t : Titans.values()) {
+			t.getTitan().onSecond();
+			if (t.getTitan().getOwner() == null) {
+				t.getTitan().resetCooldown();
+				if (t.getTitan().isTransformedinTitan()) {
+					t.getTitan().setTransformedinTitan(false);
 				}
 			}
-			if (getWarHammerCooldown()== 60*4) {
-				Titans.WarHammer.getTitan().Transfo();
-			}
-			if (getWarHammerCooldown() > 0) {
-				setWarHammerCooldown(getWarHammerCooldown()-1);
-			}
-			if (getWarHammerCooldown() == 0) {
-				if (getWarHammer() != null) {
-					Player player = Bukkit.getPlayer(getWarHammer());
-					if (player != null) {
-						player.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.WarHammer.getTitan().getName());
-					}
-					setWarHammerCooldown(-5);
-				}
-			}
-			if (getMachoireCooldown() == 0) {
-				if (getMachoire() != null) {
-					Player p = Bukkit.getPlayer(getMachoire());
-					if (p != null) {
-						p.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.Machoire.getTitan().getName());
-					}
-					setMachoireCooldown(-5);
-				}
-			}
-			if (getMachoireCooldown()>0) {
-				setMachoireCooldown(getMachoireCooldown()-1);
-			}
-			if (getMachoireCooldown() == 60*3) {
-				Titans.Machoire.getTitan().Transfo();
-			}
-			if (getAssaillantCooldown() > 0) {
-				setAssaillantCooldown(getAssaillantCooldown()-1);
-				if (getAssaillantCooldown() == 60*4) {
-					Titans.Assaillant.getTitan().Transfo();
-				}
-			}
-			if (getAssaillantCooldown() == 0) {
-				if (getAssaillant() != null) {
-					Player p = Bukkit.getPlayer(getAssaillant());
-					if (p != null) {
-						p.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.Assaillant.getTitan().getName());
-					}
-					setAssaillantCooldown(-5);
-				}
-			}
-			if (getBestialCooldown() > 0) {
-				setBestialCooldown(getBestialCooldown()-1);
-				if (getBestialCooldown() == 60*4) {
-					Titans.Bestial.getTitan().Transfo();
-				}
-			}
-			if (getBestialCooldown() == 0) {
-				if (getBestial() != null) {
-					Player p = Bukkit.getPlayer(getBestial());
-					if (p != null) {
-						p.sendMessage("§7Vous pouvez à nouveau vous transformez en Titan "+Titans.Bestial.getTitan().getName());
-					}
-					setBestialCooldown(-5);
-				}
-			}
-			for (Titans t : Titans.values()) {
-				t.getTitan().onSecond();
-				if (t.getTitan().getOwner() == null) {
-					t.getTitan().resetCooldown();
-					if (t.getTitan().isTransformedinTitan()) {
-						t.getTitan().setTransformedinTitan(false);
-					}
-				}
-			}
+		}
 	}
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onRecup(PlayerPickupItemEvent e) {
