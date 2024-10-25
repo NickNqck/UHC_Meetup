4ad1a04146affbe00980c3a48741c92c591bae06
NickNqck <jadorevolvic@gmail.com>
2024-10-25 21:37:29 +0200

    Muichiro maintenant implementer

--- src/main/java/fr/nicknqck/roles/ds/demons/lune/Gyokko.java
+++ src/main/java/fr/nicknqck/roles/ds/demons/lune/Gyokko.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import fr.nicknqck.roles.builder.TeamList;
 import fr.nicknqck.roles.ds.builders.DemonType;
 import fr.nicknqck.roles.ds.builders.DemonsRoles;
-import fr.nicknqck.roles.ds.slayers.pillier.Muichiro;
+import fr.nicknqck.roles.ds.slayers.pillier.MuichiroV2;
 import lombok.NonNull;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -159,7 +159,7 @@ public class Gyokko extends DemonsRoles {
 				if (gameState.getInGamePlayers().contains(victim.getUniqueId())) {
 					if (gameState.getPlayerRoles().containsKey(victim)) {
 						RoleBase role = gameState.getPlayerRoles().get(victim);
-						if (role instanceof Muichiro) {
+						if (role instanceof MuichiroV2) {
 							killmuichiro = true;
 							giveItem(owner, false, Items.getGyokkoBoots());
 							owner.sendMessage(ChatColor.GRAY+"Vous venez de tuez "+ChatColor.GOLD+ role.getRoles() +ChatColor.GRAY+"vous obtenez donc "+ChatColor.RED+"force 1 le jour"+ChatColor.GRAY+", ainsi que des bottes en diamant enchantée avec: "+ChatColor.GOLD+"Depht Strider 2");
