146b6cd53e26f25aba81746b7bb9d551b5fe12d3
NickNqck <jadorevolvic@gmail.com>
2024-11-10 23:03:30 +0100

    Amélioration descs

--- src/main/java/fr/nicknqck/roles/ds/slayers/KagayaV2.java
+++ src/main/java/fr/nicknqck/roles/ds/slayers/KagayaV2.java
@@ -2,13 +2,16 @@ package fr.nicknqck.roles.ds.slayers;
 
 import fr.nicknqck.GameState;
 import fr.nicknqck.Main;
+import fr.nicknqck.events.custom.UHCDeathEvent;
 import fr.nicknqck.events.custom.UHCPlayerKillEvent;
 import fr.nicknqck.player.GamePlayer;
 import fr.nicknqck.roles.builder.AutomaticDesc;
 import fr.nicknqck.roles.builder.EffectWhen;
 import fr.nicknqck.roles.builder.RoleBase;
+import fr.nicknqck.roles.desc.AllDesc;
 import fr.nicknqck.roles.ds.builders.SlayerRoles;
 import fr.nicknqck.roles.ds.builders.Soufle;
+import fr.nicknqck.roles.ds.demons.Muzan;
 import fr.nicknqck.roles.ds.slayers.pillier.PilierRoles;
 import fr.nicknqck.utils.Loc;
 import fr.nicknqck.utils.event.EventUtils;
@@ -86,16 +89,36 @@ public class KagayaV2 extends SlayerRoles {
 
         private int stade = 1;
         private final ResiMatesRunnable runnable;
+        private int rdmKiller = 25;
+        private boolean muzanDeath = false;
 
         protected MaladieItem(@NonNull KagayaV2 role) {
             super("§2Maladie", new Cooldown(10), new ItemBuilder(Material.SPIDER_EYE).setName("§2Maladie"), role,
+                    "§7Vous permet d'augmenter le niveau de votre §2maladie§7, au départ vous serez au§c stade 1§7, plus vous§c augmenterez§7 votre§2 maladie§7 plus vous aurez de§c bonus§7: ",
                     "",
-                    "Item de test",
-                    "");
+                    "§8 • §cStade 2§7:",
+                    "",
+                    AllDesc.tab+"§7Vous obtiendrez l'identité d'un§a pilier§7 choisis§c aléatoirement§7 ainsi que l'effet§9 résistance 1§7 à moins de §c20 blocs§7 de lui",
+                    AllDesc.tab+"§7Vous obtiendrez l'effet§8 weakness 1§7 de manière§c permanente§7.",
+                    "",
+                    "§8 • §cStade 3§7:",
+                    "",
+                    AllDesc.tab+"§7Vous obtiendrez la possibilité de voir la§c vie§7 des joueurs au dessus d'eux",
+                    AllDesc.tab+"§7A la mort d'un§a pilier§7 vous obtiendrez le§c rôle§7 du§c tueur",
+                    AllDesc.tab+"§7A la mort d'un§a pilier§7 vous aurez§c 25%§7 de chance d'obtenir le§c pseudo§7 du§c tueur",
+                    AllDesc.tab+"§7La porter de votre§9 résistance§7 augmente de§c 10 blocs",
+                    "",
+                    "§8 • §cStade 4§7:",
+                    "",
+                    AllDesc.tab+"§7La porter de votre§9 résistance§7 augmente de§c 10 blocs",
+                    AllDesc.tab+"§7Le pourcentage de chance pour obtenir le §cpseudo§7 du §ctueur§7 d'un pilier augmente de§c 25%",
+                    AllDesc.tab+"§7Si§c Muzan§7 est mort, vous obtiendrez§c +1/2❤ permanent§7 par§c kill§7 fait par §cvous§7 ou votre§a pilier",
+                    "",
+                    "§cA chaque stade vous perdrez 2❤ permanents");
             EventUtils.registerRoleEvent(this);
             this.runnable = new ResiMatesRunnable(this);
+            setMaxUse(3);
         }
-
         @Override
         public boolean onUse(Player player, Map<String, Object> map) {
             if (getInteractType().equals(InteractType.INTERACT)) {
@@ -107,10 +130,10 @@ public class KagayaV2 extends SlayerRoles {
             return false;
         }
         private void setStade(final int state,final Player owner) {
+            getRole().setMaxHealth(getRole().getMaxHealth()-4);
+            owner.setMaxHealth(getRole().getMaxHealth());
             switch (state) {
                 case 2:
-                    getRole().setMaxHealth(getRole().getMaxHealth()-4);
-                    owner.setMaxHealth(getRole().getMaxHealth());
                     PilierRoles pilier = this.findRandomPillier();
                     if (pilier == null) {
                         owner.sendMessage("§7Malheureusement, aucun§a pilier§7 n'est encore en§c vie§7, votre§c sacrifice§7 a donc été§c inutile.");
@@ -123,27 +146,53 @@ public class KagayaV2 extends SlayerRoles {
                     getRole().givePotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 60, 0), EffectWhen.PERMANENT);
                     break;
                 case 3:
-                    getRole().setMaxHealth(getRole().getMaxHealth()-4);
-                    owner.setMaxHealth(getRole().getMaxHealth());
                     Bukkit.getScheduler().runTaskLaterAsynchronously(getPlugin(), () -> getRole().addPower(new ShowHealthPower(getRole())), 5);
+                    this.runnable.distance = 30;
                     break;
+                case 4:
+                    this.runnable.distance = 40;
+                    this.rdmKiller = 50;
                 default:
                     break;
             }
         }
         @EventHandler
-        private void onDeath(UHCPlayerKillEvent event) {
+        private void onKill(UHCPlayerKillEvent event) {
+            if (event.isCancel())return;
             if (event.getGamePlayerKiller() != null) {
                 final GamePlayer gameVictim = event.getGameState().getGamePlayer().get(event.getVictim().getUniqueId());
                 if (gameVictim == null)return;
                 if (gameVictim.getRole() == null)return;
+                if (event.getGamePlayerKiller().getRole() == null)return;
                 if (gameVictim.getRole() instanceof PilierRoles) {
                     if (this.stade >= 3) {
                         Player owner = getPlugin().getServer().getPlayer(getRole().getPlayer());
                         if (owner != null) {
-
+                            owner.sendMessage("§7Le tueur de§a "+gameVictim.getRole().getName()+"§7 est§c "+event.getGamePlayerKiller().getRole().getName()+(Main.RANDOM.nextInt(100) <= this.rdmKiller ? " §7(§c"+event.getGamePlayerKiller().getPlayerName()+"§7)" : ""));
                         }
                     }
+                }
+                if (event.getGamePlayerKiller().getUuid().equals(getRole().getPlayer()) || event.getGamePlayerKiller().getUuid().equals(this.runnable.pilierUUID)) {
+                    if (this.stade >= 4 && this.muzanDeath) {
+                        this.getRole().setMaxHealth(this.getRole().getMaxHealth()+1);
+                        final Player owner = Bukkit.getPlayer(getRole().getPlayer());
+                        if (owner != null) {
+                            owner.setMaxHealth(getRole().getMaxHealth());
+                            owner.sendMessage("§7Vous ou votre§a pilier§7 a tuer quelqu'un, vous gagnez§c +1/2❤ permanent");
+                        }
+                    }
+                }
+            }
+        }
+        @EventHandler
+        private void onDeath(UHCDeathEvent event) {
+            if (event.isCancelled())return;
+            if (event.getRole() == null)return;
+            if (event.getRole() instanceof Muzan) {
+                this.muzanDeath = true;
+                Player owner = Bukkit.getPlayer(getRole().getPlayer());
+                if (owner != null && this.stade >= 4) {
+                    owner.sendMessage("§cMuzan§7 est§c mort§7, votre§a santé§7 va enfin pouvoir se§d régénérer§7.");
                 }
             }
         }
@@ -164,11 +213,14 @@ public class KagayaV2 extends SlayerRoles {
 
             private final KagayaV2 kagaya;
             private final GameState gameState;
+            @NonNull
             private UUID pilierUUID;
+            private int distance = 20;
 
             private ResiMatesRunnable(MaladieItem item) {
                 this.kagaya = (KagayaV2) item.getRole();
                 this.gameState = kagaya.getGameState();
+                this.pilierUUID = item.getRole().getPlayer();
             }
 
             @Override
@@ -177,12 +229,11 @@ public class KagayaV2 extends SlayerRoles {
                     cancel();
                     return;
                 }
-                if (this.pilierUUID == null) return;
                 final Player owner = Bukkit.getPlayer(kagaya.getPlayer());
                 if (owner != null) {
                     Player pilier = Bukkit.getPlayer(pilierUUID);
                     if (pilier != null) {
-                        final List<Player> aroundPlayers = Loc.getNearbyPlayersExcept(owner, 20);
+                        final List<Player> aroundPlayers = Loc.getNearbyPlayersExcept(owner, this.distance);
                         if (aroundPlayers.contains(pilier)) {
                             Bukkit.getScheduler().runTask(Main.getInstance(), () -> owner.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 60, 0, false, false), true));
                         }
