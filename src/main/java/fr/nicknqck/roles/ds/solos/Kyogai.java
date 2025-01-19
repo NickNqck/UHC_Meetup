bc4cea13683267765dfe1811538d913984808746
NickNqck <jadorevolvic@gmail.com>
2025-01-19 10:47:29 +0100

    Kyogai.java & Jigoro.java n'utilise plus de gameState.getPlayerRoles()

--- src/main/java/fr/nicknqck/roles/ds/solos/Kyogai.java
+++ src/main/java/fr/nicknqck/roles/ds/solos/Kyogai.java
@@ -97,27 +97,7 @@ public class Kyogai extends DemonsRoles {
 	public void FormChoosen(ItemStack item, GameState gameState) {
 		if (item.isSimilar(GUIItems.getKyogaiDémon())) {
 			if (!FFA.getFFA()) {
-				for (UUID u : gameState.getInGamePlayers()) {
-					Player e = Bukkit.getPlayer(u);
-					if (e == null)continue;
-					if (gameState.getPlayerRoles().get(e) != null) {
-						if (!gameState.hasRoleNull(e.getUniqueId())) {
-							for (UUID as : gameState.getInGamePlayers()) {
-								Player s = Bukkit.getPlayer(as);
-								if (s == null)continue;
-								if (!gameState.hasRoleNull(s.getUniqueId())) {
-										if (gameState.getAvailableRoles().containsKey(Roles.Muzan)) {
-											if (gameState.getPlayerRoles().get(e) instanceof Muzan) {
-												s.sendMessage("Le joueur §6"+s.getName()+"§r est§6 "+gameState.getPlayerRoles().get(e).getRoles().name());
-											}
-										} else {
-											s.sendMessage("Aucun joueur ne possède le rôle§6 Muzan");
-										}
-								}
-							}
-						}
-					}
-				}
+				getKnowedRoles().add(Muzan.class);
 				setTeam(TeamList.Demon);
 				owner.sendMessage("La commande§6 /ds me§r à été mis-à-jour !");
 			} else {
