8d84aa66924cc49088825077f7e2fb25ad574501
NickNqck <jadorevolvic@gmail.com>
2025-01-26 18:34:00 +0100

    Ajout d'un boolean dans la GameConfig.java pour donner une lame a tout le monde a l'annonce des rôles

--- src/main/java/fr/nicknqck/GameListener.java
+++ src/main/java/fr/nicknqck/GameListener.java
@@ -255,6 +255,9 @@ public class GameListener implements Listener {
                         role.RoleGiven(gameState);
                         role.GiveItems();
                         lastRoleGive = role;
+						if (Main.getInstance().getGameConfig().isGiveLame()) {
+							role.giveItem(p, false, Items.getLamedenichirin());
+						}
                         Bukkit.getPluginManager().callEvent(new RoleGiveEvent(this.gameState, role, role.getRoles(), role.getGamePlayer(), false));
                     }
                     Bukkit.getScheduler().runTaskLaterAsynchronously(Main.getInstance(), () -> p.sendMessage("§cDiscord du mode de jeu: §6https://discord.gg/6dWxCAEsfF"), 20*10);//20ticks* le nombre de seconde voulue
