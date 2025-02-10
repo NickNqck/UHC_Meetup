62ac14b000f6085ef74f87b800e6eb9a0160218b
NickNqck <jadorevolvic@gmail.com>
2025-02-10 20:57:44 +0100

    Séparation du pacte 3 du rôle original

---
+++ src/main/java/fr/nicknqck/roles/ds/solos/jigorov2/JigoroV2PZenItsu.java
@@ -0,0 +1,119 @@
+package fr.nicknqck.roles.ds.solos.jigorov2;
+
+import fr.nicknqck.GameState;
+import fr.nicknqck.Main;
+import fr.nicknqck.events.custom.UHCPlayerKillEvent;
+import fr.nicknqck.player.GamePlayer;
+import fr.nicknqck.roles.builder.EffectWhen;
+import fr.nicknqck.roles.builder.RoleBase;
+import fr.nicknqck.roles.desc.AllDesc;
+import fr.nicknqck.roles.ds.demons.lune.Kaigaku;
+import fr.nicknqck.roles.ds.slayers.ZenItsuV2;
+import fr.nicknqck.utils.ArrowTargetUtils;
+import fr.nicknqck.utils.Loc;
+import fr.nicknqck.utils.event.EventUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class JigoroV2PZenItsu extends JigoroV2 implements Listener {
+
+    private final ZenItsuV2 zenItsu;
+    private boolean killKaigaku;
+
+    public JigoroV2PZenItsu(final UUID player, final ZenItsuV2 zenItsu, final GamePlayer gamePlayer) {
+        super(player);
+        this.zenItsu = zenItsu;
+        this.killKaigaku = false;
+        setCanuseblade(true);
+        setLameincassable(true);
+        setGamePlayer(gamePlayer);
+        gamePlayer.setRole(this);
+        new ZenItsuRunnable(zenItsu, this, getGameState());
+        EventUtils.registerRoleEvent(this);
+    }
+
+    @Override
+    public String[] Desc() {
+        return AllDesc.JigoroV2Pacte3;
+    }
+
+    @EventHandler
+    private void onUHCKill(final UHCPlayerKillEvent event) {
+        if (event.getGamePlayerKiller() == null)return;
+        if (event.getGameState().hasRoleNull(event.getVictim().getUniqueId()))return;
+        if (event.getGamePlayerKiller().getUuid().equals(this.zenItsu.getPlayer()) || event.getGamePlayerKiller().getUuid().equals(getPlayer())) {
+            final RoleBase role = event.getGameState().getGamePlayer().get(event.getVictim().getUniqueId()).getRole();
+            if (role instanceof Kaigaku) {
+                givePotionEffect(new PotionEffect(PotionEffectType.SPEED, 100, 1, false, false), EffectWhen.MID_LIFE);
+                getGamePlayer().sendMessage("§6"+event.getGamePlayerKiller().getRole().getName()+"§f à tué§c Kaigaku§f ce qui vous permet d'avoir§b Speed II§f en dessous de §c5❤");
+                this.killKaigaku = true;
+            }
+        }
+    }
+
+    private static final class ZenItsuRunnable extends BukkitRunnable {
+
+        private final ZenItsuV2 zenItsu;
+        private final JigoroV2PZenItsu jigoro;
+        private final GameState gameState;
+
+        private ZenItsuRunnable(ZenItsuV2 zenItsu, JigoroV2PZenItsu jigoro, GameState gameState) {
+            this.zenItsu = zenItsu;
+            this.jigoro = jigoro;
+            this.gameState = gameState;
+        }
+
+        @Override
+        public void run() {
+            if (!gameState.getServerState().equals(GameState.ServerStates.InGame)) {
+                cancel();
+                return;
+            }
+            if (!jigoro.getGamePlayer().isAlive()) {
+                jigoro.getGamePlayer().getActionBarManager().removeInActionBar("jigoro.traquezen");
+                return;
+            }
+            if (!zenItsu.getGamePlayer().isAlive()) {
+                zenItsu.getGamePlayer().getActionBarManager().removeInActionBar("jigoro.traquezen");
+                return;
+            }
+            final Player owner = Bukkit.getPlayer(jigoro.getPlayer());
+            final Player mate = Bukkit.getPlayer(zenItsu.getPlayer());
+            if (owner == null)return;
+            if (mate == null)return;
+            if (!owner.getWorld().equals(mate.getWorld()))return;
+            final DecimalFormat df = new DecimalFormat("0");
+            jigoro.getGamePlayer().getActionBarManager().updateActionBar("jigoro.traquezen", "§aZenItsu§r: "+df.format(
+                    owner.getLocation().distance(mate.getLocation())
+            ) + ArrowTargetUtils.calculateArrow(owner, mate.getLocation()));
+            zenItsu.getGamePlayer().getActionBarManager().updateActionBar("jigoro.traquezen", "§6Jigoro§r: "+df.format(
+                    mate.getLocation().distance(owner.getLocation())
+            ) + ArrowTargetUtils.calculateArrow(mate, owner.getLocation()));
+
+            if (jigoro.killKaigaku) {
+                if (owner.getHealth() <= (owner.getMaxHealth()/2)) {
+                    owner.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 100, 1, false, false), true);
+                }
+            }
+
+            final List<Player> locs = new ArrayList<>(Loc.getNearbyPlayersExcept(owner, 15));
+            if (locs.isEmpty())return;
+            if (locs.contains(mate)) {
+                Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
+                    mate.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100, 0, false, false), true);
+                    owner.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100, 0, false, false), true);
+                });
+            }
+        }
+    }
+}
