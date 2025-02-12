8f3b5454d672a437a6d7f1d5aae3d24ab65b0e08
NickNqck <jadorevolvic@gmail.com>
2025-02-12 14:12:48 +0100

    MAJ KyogaiDemon.java peut maintenant devenir être solo quand Muzan meurt

--- src/main/java/fr/nicknqck/roles/ds/demons/KyogaiDemon.java
+++ src/main/java/fr/nicknqck/roles/ds/demons/KyogaiDemon.java
@@ -1,6 +1,8 @@
 package fr.nicknqck.roles.ds.demons;
 
 import fr.nicknqck.GameState;
+import fr.nicknqck.Main;
+import fr.nicknqck.events.custom.UHCDeathEvent;
 import fr.nicknqck.events.custom.UHCPlayerBattleEvent;
 import fr.nicknqck.events.custom.UHCPlayerKillEvent;
 import fr.nicknqck.roles.builder.AutomaticDesc;
@@ -32,10 +34,13 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 public class KyogaiDemon extends DemonsRoles implements Listener {
+
+    private boolean solo = false;
 
     public KyogaiDemon(UUID player) {
         super(player);
@@ -92,6 +97,11 @@ public class KyogaiDemon extends DemonsRoles implements Listener {
                                         "§7Il vous permettra d'activer un§c passif§7 pour une durée de§c 10 secondes\n" +
                                         "§7Ce qui vous permettra de retourner n'importe quel joueur que vous§c frapperez\n\n" +
                                         "§7(1x/10m)")
+                        }),
+                        new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{
+                                new TextComponent("§7A la mort de§c Muzan§7 vous deviendrez un rôle§e Solitaire§7, vous obtiendrez donc les§c bonus§7 suivant:\n\n" +
+                                        "§7Vous obtiendrez l'effet§9 Résistance I§7 de manière§c permanente§7 ainsi que§c 3❤ permanents\n\n" +
+                                        "§7Votre§c Tambour§7 aura §c50%§7 de §cchance§7 de retourner tout les joueurs autours de vous (§c30 blocs§7)")
                         })
                 ).getText();
     }
@@ -108,10 +118,26 @@ public class KyogaiDemon extends DemonsRoles implements Listener {
             event.getGamePlayerKiller().sendMessage("§7Vous avez réussis à tué "+role.getName()+"§7, vous obtenez donc finalement la faculté d'utiliser vos§6 Percussion Rapide");
         }
     }
+    @EventHandler
+    private void UHCDeathEvent(final UHCDeathEvent event) {
+        if (event.getRole() instanceof Muzan) {
+            this.solo = true;
+            getGamePlayer().sendMessage("§7Vous êtes enfin libéré de l'emprise de§c Muzan§7 vous obtenez l'effet§9 Résistance I§c permanent§7 ainsi que§c 3❤ permanents§7",
+                    "§7Votre§c Tambour§7 à maintenant§c 50%§7 de§c chance§7 de retourner tout les joueurs autours de vous (§c30 blocs§7)");
+            givePotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 100, 0, false, false), EffectWhen.PERMANENT);
+            setMaxHealth(getMaxHealth()+6.0);
+            final Player owner = Bukkit.getPlayer(getPlayer());
+            if (owner != null) {
+                owner.setMaxHealth(getMaxHealth());
+                owner.setHealth(owner.getHealth()+6.0);
+            }
+            setTeam(TeamList.Solo);
+        }
+    }
 
     private static class TambourPower extends ItemPower {
 
-        protected TambourPower(@NonNull RoleBase role) {
+        protected TambourPower(@NonNull KyogaiDemon role) {
             super("Tambour", new Cooldown(30), new ItemBuilder(Material.STICK).setName("§cTambour"), role);
         }
 
@@ -121,6 +147,22 @@ public class KyogaiDemon extends DemonsRoles implements Listener {
             if (target == null) {
                 player.sendMessage("§cIl faut viser un joueur !");
                 return false;
+            }
+            boolean skip = false;
+            if (((KyogaiDemon) getRole()).solo) {
+                if (Main.RANDOM.nextInt(101) <= 50)skip = true;
+                final List<Player> aroundPlayers = Loc.getNearbyPlayersExcept(player, 30);
+                final StringBuilder stringBuilder = new StringBuilder();
+                if (aroundPlayers.size() <= 1) skip = true;
+                if (!skip) {
+                    for (final Player p : aroundPlayers) {
+                        stringBuilder.append("§c").append(p.getDisplayName()).append("§7, ");
+                        Loc.inverserDirectionJoueur(p);
+                        target.sendMessage("§cKyogai§7 vous a retourné");
+                    }
+                    player.sendMessage("§7Vous avez retourné les joueurs: "+ stringBuilder.substring(3));
+                    return true;
+                }
             }
             Loc.inverserDirectionJoueur(target);
             player.sendMessage("§7Vous avez retourné§c "+target.getDisplayName());
