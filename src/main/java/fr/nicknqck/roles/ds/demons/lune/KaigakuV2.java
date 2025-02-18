3e92efd505ff7a05a6af18b0b93472b5a4ebc57a
NickNqck <jadorevolvic@gmail.com>
2025-02-18 21:57:46 +0100

    Ajout d'un nouveau pouvoir

--- src/main/java/fr/nicknqck/roles/ds/demons/lune/KaigakuV2.java
+++ src/main/java/fr/nicknqck/roles/ds/demons/lune/KaigakuV2.java
@@ -1,6 +1,7 @@
 package fr.nicknqck.roles.ds.demons.lune;
 
 import fr.nicknqck.GameState;
+import fr.nicknqck.Main;
 import fr.nicknqck.events.custom.DayEvent;
 import fr.nicknqck.events.custom.NightEvent;
 import fr.nicknqck.events.custom.UHCPlayerKillEvent;
@@ -20,6 +21,8 @@ import fr.nicknqck.utils.powers.ItemPower;
 import fr.nicknqck.utils.powers.Power;
 import fr.nicknqck.utils.raytrace.RayTrace;
 import lombok.NonNull;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -35,7 +38,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -87,7 +92,22 @@ public class KaigakuV2 extends DemonsRoles {
 
     @Override
     public TextComponent getComponent() {
-        return new AutomaticDesc(this).addEffects(getEffects()).setPowers(getPowers()).getText();
+        return new AutomaticDesc(this).addEffects(getEffects()).setPowers(getPowers()).addParticularites(
+                new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{
+                        new TextComponent("§7Pour charger votre§c charge§7 d'§cÉlectrokinésie§7 vous pourrez faire ces choses:\n\n" +
+                                "§7 -§a Rester en vie pendant§c 1 minute§7:§a +1%\n" +
+                                "§7 -§c Infliger§7/§cRecevoir§7 un§c coup d'épée§7:§a +1%\n" +
+                                "§7 -§a Manger une§e pomme d'or§7:§a +1%\n" +
+                                "§7 -§a Parcourir§c 150 blocs§7:§a +5%\n" +
+                                "§7 -§c Tuer§7 un§c joueur§7:§a +10%")
+                }),
+                new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{
+                        new TextComponent("§7Si vous tué§a Zen'Itsu§7 votre§c Électrokinésie§7 aura des changements:\n\n" +
+                                "§7 - Votre§c clique droit§7 fera§c toujours§7 les mêmes effet que s'il est à pleine charge\n" +
+                                "§7 - Le dernier palier de charge aura un autre pouvoir qui s'activera en essayant de§c drop§7 votre§c item§7:\n\n" +
+                                "§7     → Créera une ligne d'§eéclair§7 qui infligera§c 2❤§7 à tout les joueurs qui seront touché")
+                })
+        ).getText();
     }
 
     private static class ElectroKinesiePower extends ItemPower implements Listener {
@@ -96,31 +116,30 @@ public class KaigakuV2 extends DemonsRoles {
         private double deplacement = 0.0;
         private final TPPower tpPower;
         private final FoudreZonePower foudrePower;
+        private final LigneEclairPower lignePower;
         private boolean firstGive = false;
         private boolean secondGive = false;
 
         protected ElectroKinesiePower(@NonNull RoleBase role) {
-            super("Électrokinésie", new Cooldown(1), new ItemBuilder(Material.BLAZE_ROD).setName("§eÉlectrokinésie"), role,
+            super("Électrokinésie", new Cooldown(30), new ItemBuilder(Material.BLAZE_ROD).setName("§eÉlectrokinésie"), role,
                     "§7Vous permet d'utiliser un§c pourcentage§7 de§c charge§7, vous aurez plusieurs bonus en fonction de votre§c chargement§7 actuel",
                     "§7pour la remplir voir votre§c particularité 1§7:",
                     "",
                     "§7 -§c 0%§7: Aucun bonus",
-                    "§7 -§c 20%§7: Via un clique gauche, vous permettra de vous téléportez derrière le joueur viser",
+                    "§7 -§c 20%§7: (§fClique gauche§7) Vous permettra de vous téléportez derrière le joueur viser en lui infligeant§c 1❤§7 de dégat",
                     "§7 -§6 40%§7: Vous irez§c 10%§7 plus§c vite§7 pendant la§c nuit",
-                    "§7 -§6 60%§7: Via un clique droit, vous§c infligerez 2❤§7 de§c dégats§7 à tout les joueurs proche (§c25 blocs§7)",
+                    "§7 -§6 60%§7: (§fClique droit§7) Vous§c infligerez 2❤§7 de§c dégats§7 à tout les joueurs proche (§c25 blocs§7)",
                     "§7 -§a 80%§7: Vous irez§c 20%§7 plus§c vite§7 pendant la§c nuit",
                     "§7 -§a 100%§7: Votre clique droit (en plus du bonus précédent) infligera l'effet§c Slowness I§7 et§6 enflammera§7 les§c joueurs§7 touchés pendant§c 10 secondes"
             );
             this.tpPower = new TPPower(role);
             this.foudrePower = new FoudreZonePower(this);
-            role.addPower(tpPower);
+            this.lignePower = new LigneEclairPower(role);
+            role.addPower(lignePower);
             role.addPower(foudrePower);
             EventUtils.registerRoleEvent(this);
             getRole().getGamePlayer().getActionBarManager().addToActionBar("kaigaku.electro", "§bCharge actuel: "+charge+"%");
             new LifeRunnable(role.getGamePlayer(), this, role.getGameState());
-            setShowCdInDesc(false);
-            setSendCooldown(false);
-            setWorkWhenInCooldown(true);
             getShowCdRunnable().setCustomText(true);
             getShowCdRunnable().setCustomTexte("");
         }
@@ -134,7 +153,7 @@ public class KaigakuV2 extends DemonsRoles {
                         player.sendMessage("§cVous n'avez pas asser d'§eélectrécité§c dans votre corp pour cette technique...");
                         return false;
                     }
-                    if (this.tpPower.checkUse(player, map)) {
+                    if (this.lignePower.checkUse(player, map)) {
                         addCharge(-20);
                         return true;
                     }
@@ -291,8 +310,9 @@ public class KaigakuV2 extends DemonsRoles {
         private static class TPPower extends Power {
 
             public TPPower(@NonNull RoleBase role) {
-                super("Électrokinésie (Clique gauche)", new Cooldown(60*5), role);
+                super("Électrokinésie (Clique gauche)", new Cooldown(5), role);
                 setShowInDesc(false);
+                setSendCooldown(false);
             }
 
             @Override
@@ -307,6 +327,12 @@ public class KaigakuV2 extends DemonsRoles {
                 loc.setZ(loc.getZ()+Math.sin(Math.toRadians(target.getEyeLocation().getYaw()-90)));
                 loc.setPitch(0f);
                 loc.getWorld().strikeLightningEffect(loc);
+                player.teleport(loc);
+                if (target.getHealth()-2.0 <= 0.0) {
+                    target.setHealth(0.1);
+                } else {
+                    target.setHealth(target.getHealth()-2.0);
+                }
                 target.sendMessage("§cVous sentez un§e éclair§c parcourir votre corp");
                 player.sendMessage("§eSoufle de la foudre: Quatrième Mouvement !");
                 return true;
@@ -317,14 +343,19 @@ public class KaigakuV2 extends DemonsRoles {
             private final ElectroKinesiePower power;
 
             public FoudreZonePower(@NonNull ElectroKinesiePower power) {
-                super("Électrokinésie (Clique droit)", new Cooldown(60*8), power.getRole());
+                super("Électrokinésie (Clique droit)", new Cooldown(5), power.getRole());
                 this.power = power;
                 setShowInDesc(false);
+                setSendCooldown(false);
             }
 
             @Override
             public boolean onUse(Player player, Map<String, Object> map) {
                 final List<Player> aroundPlayers = Loc.getNearbyPlayersExcept(player, 25);
+                if (aroundPlayers.isEmpty()) {
+                    player.sendMessage("§cIl n'y a pas assez de joueurs autours de vous pour cette technique !");
+                    return false;
+                }
                 for (final Player target : aroundPlayers) {
                     target.getWorld().strikeLightningEffect(target.getLocation());
                     target.setHealth(Math.max(0.1, target.getHealth()-4.0));
@@ -340,6 +371,73 @@ public class KaigakuV2 extends DemonsRoles {
                     this.power.addCharge(-60);
                 }
                 return true;
+            }
+        }
+        private static class LigneEclairPower extends Power {
+
+            public LigneEclairPower(@NonNull RoleBase role) {
+                super("Électrokinésie (drop)", new Cooldown(5), role);
+                setShowInDesc(false);
+                setSendCooldown(false);
+            }
+
+            @Override
+            public boolean onUse(Player player, Map<String, Object> map) {
+                player.sendMessage("CACOUCACOU "+isSendCooldown());
+                new LigneRunnable(getRole().getGameState(), player);
+                return true;
+            }
+            private static class LigneRunnable extends BukkitRunnable {
+
+                private final GameState gameState;
+                private final Location location;
+                private final List<UUID> damaged;
+                private final UUID user;
+                private int foudreLeft;
+                private final Vector initDirection;
+
+                private LigneRunnable(GameState gameState, final Player player) {
+                    this.gameState = gameState;
+                    this.location = player.getEyeLocation();
+                    this.user = player.getUniqueId();
+                    this.foudreLeft = 30;
+                    this.damaged = new ArrayList<>();
+                    this.initDirection = this.location.getDirection().normalize().multiply(2);
+                    runTaskTimer(Main.getInstance(), 0, 15);
+                }
+
+                @Override
+                public void run() {
+                    if (!gameState.getServerState().equals(GameState.ServerStates.InGame)) {
+                        cancel();
+                        return;
+                    }
+                    location.add(initDirection);
+                    location.setY(location.getWorld().getHighestBlockYAt(location));
+                    location.getWorld().strikeLightningEffect(location);
+                    for (int y = 50; y < 128; y++) {
+                        final Location loc = new Location(location.getWorld(), location.getBlockX(), y, location.getBlockZ());
+                        final List<Player> players = Loc.getNearbyPlayers(loc, 5.0);
+                        if (players.isEmpty())continue;
+                        for (final Player player : players) {
+                            if (player.getUniqueId().equals(this.user))continue;
+                            if (!this.damaged.contains(player.getUniqueId())) {
+                                if (player.getHealth()-4.0 <= 0.0) {
+                                    player.setHealth(0.1);
+                                } else {
+                                    player.setHealth(player.getHealth()-4.0);
+                                }
+                                this.damaged.add(player.getUniqueId());
+                            }
+                        }
+                    }
+                    this.damaged.clear();
+                    if (foudreLeft <= 0) {
+                        cancel();
+                        return;
+                    }
+                    foudreLeft--;
+                }
             }
         }
     }
