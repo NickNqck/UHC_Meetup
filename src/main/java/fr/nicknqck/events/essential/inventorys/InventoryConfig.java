b5e72a2c03e417c41e6d19af9efef1cdfe390d16
NickNqck <jadorevolvic@gmail.com>
2025-03-01 20:30:33 +0100

    Simplification

--- src/main/java/fr/nicknqck/events/essential/inventorys/InventoryConfig.java
+++ src/main/java/fr/nicknqck/events/essential/inventorys/InventoryConfig.java
@@ -127,13 +127,13 @@ public class InventoryConfig implements Listener {
             if (item.isSimilar(GUIItems.getEnderPearl())) {
                 if (action.equals(InventoryAction.PICKUP_ALL)) {
                     if (Main.getInstance().getGameConfig().getStuffConfig().getPearl() == 0) {
-                        Main.getInstance().getGameConfig().getStuffConfig().setPearl(Main.getInstance().getGameConfig().getStuffConfig().getPearl()+1);
+                        Main.getInstance().getGameConfig().getStuffConfig().setPearl(1);
                     } else {
                         player.sendMessage("Vous avez déjà atteint la limite maximum d'ender pearl");
                     }
                 } else if (action.equals(InventoryAction.PICKUP_HALF)) {
                     if (Main.getInstance().getGameConfig().getStuffConfig().getPearl() == 1) {
-                        Main.getInstance().getGameConfig().getStuffConfig().setPearl(Main.getInstance().getGameConfig().getStuffConfig().getPearl()-1);
+                        Main.getInstance().getGameConfig().getStuffConfig().setPearl(0);
                     } else {
                         player.sendMessage("Vous avez déjà atteint la limite minimum d'ender pearl");
                     }
