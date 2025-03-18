2fc7cb00ca377e3f10c409608beb917d0b5c1dbc
NickNqck <jadorevolvic@gmail.com>
2025-03-18 11:53:47 +0100

    Ajout de 2 Interface

---
+++ src/main/java/fr/nicknqck/roles/krystal/BonusKrystalBase.java
@@ -0,0 +1,68 @@
+package fr.nicknqck.roles.krystal;
+
+import fr.nicknqck.GameState;
+import fr.nicknqck.Main;
+import fr.nicknqck.roles.builder.AutomaticDesc;
+import fr.nicknqck.roles.builder.EffectWhen;
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.UUID;
+
+@Getter
+public abstract class BonusKrystalBase extends KrystalBase implements IKrystalRoleBooster {
+
+    private final KrystalBonusManager krystalBonusManager;
+
+    public BonusKrystalBase(UUID player) {
+        super(player);
+        this.krystalBonusManager = new KrystalBonusManager(this);
+    }
+
+    @Override
+    public String getBonusString() {
+        final StringBuilder sb = new StringBuilder("§7");
+        if (!getBonus().isEmpty()) {
+            for (final PotionEffect potionEffect : getBonus().keySet()) {
+                sb.append("Vous obtiendrez l'effet§c ")
+                        .append(AutomaticDesc.getPotionEffectNameWithRomanLevel(potionEffect))
+                        .append("§7 en ayant au§c minimum§d ")
+                        .append(getBonus().get(potionEffect))
+                        .append(" krystaux");
+            }
+        }
+        return sb.toString();
+    }
+
+    @Getter
+    private static class KrystalBonusManager extends BukkitRunnable {
+
+        private final GameState gameState;
+        private final BonusKrystalBase role;
+
+        private KrystalBonusManager(BonusKrystalBase role) {
+            this.gameState = role.getGameState();
+            this.role = role;
+            runTaskTimerAsynchronously(Main.getInstance(), 0, 20);
+        }
+
+        @Override
+        public void run() {
+            if (!gameState.getServerState().equals(GameState.ServerStates.InGame)) {
+                cancel();
+                return;
+            }
+            if (role.getBonus().isEmpty())return;
+            final Player owner = Bukkit.getPlayer(this.role.getPlayer());
+            if (owner == null)return;
+            for (final PotionEffect potionEffect : this.role.getBonus().keySet()) {
+                if (this.role.getKrystalAmount() >= this.role.getBonus().get(potionEffect)) {
+                    Bukkit.getScheduler().runTask(Main.getInstance(), () -> this.role.givePotionEffect(potionEffect, EffectWhen.NOW));
+                }
+            }
+        }
+    }
+}
