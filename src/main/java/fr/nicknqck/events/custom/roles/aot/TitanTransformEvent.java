873f285cb23efaa5551c0ad3b029bb2cd0d49ff1
NickNqck <jadorevolvic@gmail.com>
2025-03-27 23:57:35 +0100

    Ajout d'un Player

--- src/main/java/fr/nicknqck/events/custom/roles/aot/TitanTransformEvent.java
+++ src/main/java/fr/nicknqck/events/custom/roles/aot/TitanTransformEvent.java
@@ -4,16 +4,19 @@ import fr.nicknqck.events.custom.GameEvent;
 import fr.nicknqck.titans.TitanBase;
 import lombok.Getter;
 import lombok.NonNull;
+import org.bukkit.entity.Player;
 
 @Getter
 public class TitanTransformEvent extends GameEvent {
 
     private final TitanBase titan;
     private final boolean transforming;
+    private final Player player;
 
-    public TitanTransformEvent(@NonNull final TitanBase titan,final boolean transming) {
+    public TitanTransformEvent(@NonNull final TitanBase titan, final boolean transming, Player player) {
         super();
         this.titan = titan;
         this.transforming = transming;
+        this.player = player;
     }
 }
