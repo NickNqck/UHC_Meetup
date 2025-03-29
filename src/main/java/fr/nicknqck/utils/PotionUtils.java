a857d950982e6417882264efd09092d10d613fb5
NickNqck <jadorevolvic@gmail.com>
2025-03-29 18:54:52 +0100

    Ajout d'un NoFall temporaire

--- src/main/java/fr/nicknqck/utils/PotionUtils.java
+++ src/main/java/fr/nicknqck/utils/PotionUtils.java
@@ -1,7 +1,8 @@
 package fr.nicknqck.utils;
 
-import java.util.ArrayList;
+import java.util.*;
 
+import lombok.NonNull;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,6 +17,8 @@ public class PotionUtils implements Listener {
     private static final ArrayList<Player> noFall = new ArrayList<>();
     private static final ArrayList<Player> invincible = new ArrayList<>();
     private static final ArrayList<Player> freeze = new ArrayList<>();
+    private static final Map<UUID, Integer> tempNoFall = new HashMap<>();
+
     public static void effectGive(Player p, PotionEffectType e, int t, int l){
         p.addPotionEffect(new PotionEffect(e, t * 20, l, true, false));
     }
@@ -48,6 +51,10 @@ public class PotionUtils implements Listener {
         freeze.remove(p);
     }
 
+    public static void addTempNoFall(@NonNull final UUID futureNoFallUUID, @NonNull final Integer amountNoFallProc) {
+        tempNoFall.put(futureNoFallUUID, amountNoFallProc);
+    }
+
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onFallDamage(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player && event.getCause() == DamageCause.FALL) {
@@ -55,6 +62,15 @@ public class PotionUtils implements Listener {
             if (noFall.contains(p) || invincible.contains(p)) {
                 event.setCancelled(true);
                 System.out.println("cancelled damage of "+p.getName());
+            }
+            if (tempNoFall.containsKey(p.getUniqueId())) {
+                int amount = tempNoFall.get(p.getUniqueId());
+                tempNoFall.remove(p.getUniqueId(), amount);
+                if (amount > 0) {
+                    event.setDamage(0.0);
+                    event.setCancelled(true);
+                    addTempNoFall(p.getUniqueId(), amount-1);
+                }
             }
         }else {
         	if (event.getEntity() instanceof Player) {
