725c8e4638cd36c6c62a7f217e9099f9098beab9
NickNqck <jadorevolvic@gmail.com>
2025-03-31 19:30:06 +0200

    Ajout du nouveau système de titan au roles

--- src/main/java/fr/nicknqck/roles/aot/builders/MahrRoles.java
+++ src/main/java/fr/nicknqck/roles/aot/builders/MahrRoles.java
@@ -1,11 +1,19 @@
 package fr.nicknqck.roles.aot.builders;
 
 import fr.nicknqck.roles.builder.TeamList;
+import fr.nicknqck.titans.TitanBase;
+import lombok.Getter;
 import lombok.NonNull;
+import lombok.Setter;
 
 import java.util.UUID;
 
+@Setter
+@Getter
 public abstract class MahrRoles extends AotRoles{
+
+    private TitanBase titan;
+
     public MahrRoles(UUID player) {
         super(player);
     }
