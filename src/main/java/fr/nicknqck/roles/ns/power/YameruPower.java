997ac7ea9af6e1c9050daa7ad320a3b3d0171c63
NickNqck <jadorevolvic@gmail.com>
2025-04-12 13:25:57 +0200

    Création du pouvoir du /ns yameru <joueur> mais en tant que pouvoir

---
+++ src/main/java/fr/nicknqck/roles/ns/power/YameruPower.java
@@ -0,0 +1,34 @@
+package fr.nicknqck.roles.ns.power;
+
+import fr.nicknqck.roles.builder.RoleBase;
+import fr.nicknqck.utils.powers.CommandPower;
+import fr.nicknqck.utils.powers.KamuiUtils;
+import lombok.NonNull;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+
+public class YameruPower extends CommandPower {
+
+    public YameruPower(@NonNull RoleBase role) {
+        super("/ns yameru <joueur>", "yameru", null, role, CommandType.NS);
+    }
+
+    @Override
+    public boolean onUse(@NonNull Player player, @NonNull Map<String, Object> map) {
+        @NonNull final String[] args = (String[]) map.get("args");
+        if (args.length == 2) {
+            final Player target = Bukkit.getPlayer(args[1]);
+            if (target == null) {
+                player.sendMessage("§CVeuiller cibler un joueur éxistant !");
+            } else {
+                if (target.getWorld().equals(Bukkit.getWorld("Kamui"))) {
+                    KamuiUtils.end(target);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
