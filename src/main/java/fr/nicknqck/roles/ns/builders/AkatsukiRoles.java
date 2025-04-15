a0791dfb6ac73eb7b52dbecc478404cd9777b244
NickNqck <jadorevolvic@gmail.com>
2025-04-15 14:18:53 +0200

    le camp Akatsuki connait maintenant sois nagato sois konan

--- src/main/java/fr/nicknqck/roles/ns/builders/AkatsukiRoles.java
+++ src/main/java/fr/nicknqck/roles/ns/builders/AkatsukiRoles.java
@@ -1,6 +1,10 @@
 package fr.nicknqck.roles.ns.builders;
 
+import fr.nicknqck.GameState;
+import fr.nicknqck.Main;
 import fr.nicknqck.roles.builder.TeamList;
+import fr.nicknqck.roles.ns.akatsuki.Konan;
+import fr.nicknqck.roles.ns.akatsuki.Nagato;
 import lombok.NonNull;
 import org.bukkit.entity.Player;
 
@@ -14,5 +18,17 @@ public abstract class AkatsukiRoles extends NSRoles{
     @Override
     public @NonNull TeamList getOriginTeam() {
         return TeamList.Akatsuki;
+    }
+
+    @Override
+    public void RoleGiven(GameState gameState) {
+        if (!(this instanceof Nagato) && !(this instanceof Konan)) {
+            if (Main.RANDOM.nextInt(101) <= 50) {
+                addKnowedRole(Nagato.class);
+            } else {
+                addKnowedRole(Konan.class);
+            }
+        }
+        super.RoleGiven(gameState);
     }
 }
