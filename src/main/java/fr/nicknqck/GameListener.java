5d21147a0180bf84f724cd5cd6772916954a22eb
NickNqck <jadorevolvic@gmail.com>
2025-04-15 16:53:25 +0200

    Fix d'une NullPointerException

--- src/main/java/fr/nicknqck/GameListener.java
+++ src/main/java/fr/nicknqck/GameListener.java
@@ -769,6 +769,7 @@ public class GameListener implements Listener {
 			ItemStack itemstack = event.getItem();
 			if (!gameState.hasRoleNull(player.getUniqueId())) {
 				for (Power power : gameState.getGamePlayer().get(player.getUniqueId()).getRole().getPowers()) {
+					if (power == null)continue;
 					if (power instanceof ItemPower) {
 						if (((ItemPower) power).getItem().isSimilar(event.getItem())) {
 							event.setCancelled(true);
