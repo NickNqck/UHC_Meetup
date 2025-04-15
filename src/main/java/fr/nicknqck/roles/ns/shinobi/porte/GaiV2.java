e5b19e3023197fc758aa11cb3273d736be05341b
NickNqck <jadorevolvic@gmail.com>
2025-04-15 05:25:05 +0200

    Simplification et utilisation de l'AutomaticDesc dynamic

--- src/main/java/fr/nicknqck/roles/ns/shinobi/porte/GaiV2.java
+++ src/main/java/fr/nicknqck/roles/ns/shinobi/porte/GaiV2.java
@@ -3,16 +3,11 @@ package fr.nicknqck.roles.ns.shinobi.porte;
 import fr.nicknqck.GameState;
 import fr.nicknqck.roles.builder.AutomaticDesc;
 import lombok.NonNull;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.inventory.ItemStack;
 
 import java.util.UUID;
 
 public class GaiV2 extends PortesRoles{
-
-    private TextComponent textComponent;
 
     public GaiV2(UUID player) {
         super(player);
@@ -22,8 +17,6 @@ public class GaiV2 extends PortesRoles{
     public void RoleGiven(GameState gameState) {
         super.RoleGiven(gameState);
         setChakraType(getRandomChakras());
-        AutomaticDesc automaticDesc = new AutomaticDesc(this).setItems(troisPorteMap(), sixPorteMap(), huitPorteMap()).addParticularites(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new BaseComponent[]{new TextComponent("§7Vous possédez une nature de chakra aléatoire, cette partie vous avez la nature de chakra: "+getChakras().getShowedName())}));
-        this.textComponent = automaticDesc.getText();
         addPower(new TroisPortePower(this), true);
         addPower(new SixPortesPower(this), true);
         addPower(new HuitPortesPower(this), true);
@@ -40,12 +33,7 @@ public class GaiV2 extends PortesRoles{
     }
 
     @Override
-    public ItemStack[] getItems() {
-        return new ItemStack[0];
-    }
-
-    @Override
     public TextComponent getComponent() {
-        return textComponent;
+        return new AutomaticDesc(this).setItems(troisPorteMap(), sixPorteMap(), huitPorteMap()).getText();
     }
 }
