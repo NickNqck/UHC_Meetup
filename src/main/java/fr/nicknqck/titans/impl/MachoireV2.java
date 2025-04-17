e4682b5f011c75ac643108fff9d84cbae2f02287
NickNqck <jadorevolvic@gmail.com>
2025-04-17 15:46:48 +0200

    Ajout d'une fonctionnalité secrète sur le jump et de la no fall au titan machoire (transformé)

--- src/main/java/fr/nicknqck/titans/impl/MachoireV2.java
+++ src/main/java/fr/nicknqck/titans/impl/MachoireV2.java
@@ -4,6 +4,7 @@ import fr.nicknqck.events.custom.roles.aot.PrepareTitanStealEvent;
 import fr.nicknqck.player.GamePlayer;
 import fr.nicknqck.roles.builder.RoleBase;
 import fr.nicknqck.titans.TitanBase;
+import fr.nicknqck.utils.event.EventUtils;
 import fr.nicknqck.utils.itembuilder.ItemBuilder;
 import fr.nicknqck.utils.powers.Cooldown;
 import fr.nicknqck.utils.powers.ItemPower;
@@ -11,6 +12,10 @@ import lombok.NonNull;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.util.Vector;
@@ -19,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class MachoireV2 extends TitanBase {
+public class MachoireV2 extends TitanBase implements Listener {
 
     private final List<PotionEffect> effects;
 
@@ -28,13 +33,14 @@ public class MachoireV2 extends TitanBase {
         this.effects = new ArrayList<>();
         this.effects.add(new PotionEffect(PotionEffectType.SPEED, 60, 0, false, false));
         gamePlayer.getRole().addPower(new JumpPower(gamePlayer.getRole(), this), true);
+        EventUtils.registerRoleEvent(this);
     }
 
     @Override
     public @NonNull String getParticularites() {
         return "§8 -§7 Votre transformation à une durée de§c 3 minutes§7.\n"+
                 " \n"+
-                "§8 -§7 Lorsque vous êtes transformé en titan vous avez l'effet§c Speed I§7.\n"+
+                "§8 -§7 Lorsque vous êtes transformé en titan vous avez l'effet§c Speed I§7 ainsi que§a No Fall§7.\n"+
                 " \n"+
                 "§8 -§7 Vous pouvez utiliser votre item \"§aSaut§7\" pour effectuer un grand bon en avant.\n"+
                 " \n"+
@@ -65,13 +71,25 @@ public class MachoireV2 extends TitanBase {
     public @NonNull PrepareTitanStealEvent.TitanForm getTitanForm() {
         return PrepareTitanStealEvent.TitanForm.MACHOIRE;
     }
+
+    @EventHandler
+    private void onDamage(@NonNull final EntityDamageEvent event) {
+        if (event.getCause().equals(EntityDamageEvent.DamageCause.FALL)) {
+            if (!isTransformed())return;
+            event.setDamage(0.0);
+            event.setCancelled(true);
+        }
+    }
+
     private static class JumpPower extends ItemPower {
 
         private final MachoireV2 machoire;
 
         protected JumpPower(@NonNull RoleBase role, MachoireV2 machoire) {
-            super("Saut", new Cooldown(30), new ItemBuilder(Material.SLIME_BALL).setName("§aSaut").addEnchant(Enchantment.DAMAGE_ALL, 1).hideEnchantAttributes(), role);
+            super("Saut", new Cooldown(25), new ItemBuilder(Material.SLIME_BALL).setName("§aSaut").addEnchant(Enchantment.DAMAGE_ALL, 1).hideEnchantAttributes(), role,
+                    "§7Les informations sur ce pouvoirs sont contenue dans votre§6 /aot info");
             this.machoire = machoire;
+            setShowInDesc(false);
         }
 
         @Override
@@ -81,9 +99,15 @@ public class MachoireV2 extends TitanBase {
                     player.sendMessage("§cIl faut être transformé en titan pour utiliser ce pouvoir");
                     return false;
                 }
+                @NonNull final PlayerInteractEvent event = (PlayerInteractEvent) map.get("event");
                 @NonNull final Vector direction = player.getLocation().getDirection();
-                direction.setY(0.8);
-                player.setVelocity(direction.multiply(1.35));
+                if (event.getAction().name().contains("LEFT")) {
+                    direction.setY(0.8);
+                    player.setVelocity(direction.multiply(1.35));
+                } else {
+                    direction.setY(0.5);
+                    player.setVelocity(direction.multiply(1.85));
+                }
                 player.sendMessage("§aJump !");
                 return true;
             }
