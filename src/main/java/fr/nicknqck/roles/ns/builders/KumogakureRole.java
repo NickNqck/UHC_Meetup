4ee2ab9e69b88cd42a98e2e8e9290033c449ad74
NickNqck <jadorevolvic@gmail.com>
2025-04-25 16:31:04 +0200

    Fix Kyubi (bug de speed)

--- src/main/java/fr/nicknqck/roles/ns/builders/KumogakureRole.java
+++ src/main/java/fr/nicknqck/roles/ns/builders/KumogakureRole.java
@@ -2,12 +2,14 @@ package fr.nicknqck.roles.ns.builders;
 
 import fr.nicknqck.GameState;
 import fr.nicknqck.Main;
+import fr.nicknqck.events.custom.EffectGiveEvent;
 import fr.nicknqck.player.GamePlayer;
 import fr.nicknqck.roles.builder.EffectWhen;
 import fr.nicknqck.roles.builder.RoleBase;
 import fr.nicknqck.roles.builder.TeamList;
 import fr.nicknqck.roles.ns.Intelligence;
 import fr.nicknqck.utils.StringUtils;
+import fr.nicknqck.utils.event.EventUtils;
 import fr.nicknqck.utils.itembuilder.ItemBuilder;
 import fr.nicknqck.utils.powers.Cooldown;
 import fr.nicknqck.utils.powers.ItemPower;
@@ -15,6 +17,8 @@ import lombok.NonNull;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -38,7 +42,7 @@ public abstract class KumogakureRole extends NSRoles {
         return Intelligence.PEUINTELLIGENT;
     }
     public abstract void onEndKyubi();
-    public static class KyubiPower extends ItemPower {
+    public static class KyubiPower extends ItemPower implements Listener {
 
         public KyubiPower(@NonNull RoleBase role) {
             super("Kyubi (Kumogakure)", new Cooldown(60*15), new ItemBuilder(Material.NETHER_STAR).setName("§6Kyubi"), role,
@@ -46,16 +50,26 @@ public abstract class KumogakureRole extends NSRoles {
                     "§8 - §aPremière minute§7: Vous obtenez les effets§e Speed II§7 ainsi que§c Force I§7.",
                     "§8 - §6Deuxième minute§7: Vous obtenez les effets§e Speed I§7 ainsi que§c Force I§7.",
                     "§8 - §cTroisième minute§7: Vous obtenez l'effet§e Speed I§7.");
+            EventUtils.registerRoleEvent(this);
         }
 
         @Override
         public boolean onUse(@NonNull Player player, @NonNull Map<String, Object> map) {
             if (getInteractType().equals(InteractType.INTERACT)) {
                 new KyubiRunnable(getRole().getGameState(), getRole().getGamePlayer()).runTaskTimerAsynchronously(Main.getInstance(), 0, 20);
-                this.getRole().getEffects().remove(new PotionEffect(PotionEffectType.SPEED, 60, 0, false, false));
                 return true;
             }
             return false;
+        }
+        @EventHandler
+        private void onEffectGive(@NonNull final EffectGiveEvent event) {
+            if (event.isCancelled())return;
+            if (!event.getPlayer().getUniqueId().equals(this.getRole().getPlayer()))return;
+            if (event.getEffectWhen().equals(EffectWhen.NOW))return;
+            if (!event.getRole().getGamePlayer().getActionBarManager().containsKey("kyubi.runnable"))return;
+            if (event.getPotionEffect().getType().equals(PotionEffectType.SPEED)) {
+                event.setCancelled(true);
+            }
         }
         private static class KyubiRunnable extends BukkitRunnable {
 
