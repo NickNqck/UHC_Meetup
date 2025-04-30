82f7f8e31be745ed8f20f4039b55874f6a1875c0
NickNqck <jadorevolvic@gmail.com>
2025-04-30 05:35:44 +0200

    Création d'un TeamChangeEvent.java

---
+++ src/main/java/fr/nicknqck/events/custom/roles/TeamChangeEvent.java
@@ -0,0 +1,30 @@
+package fr.nicknqck.events.custom.roles;
+
+import fr.nicknqck.events.custom.GameEvent;
+import fr.nicknqck.roles.builder.RoleBase;
+import fr.nicknqck.roles.builder.TeamList;
+import org.bukkit.event.Cancellable;
+
+public class TeamChangeEvent extends GameEvent implements Cancellable {
+
+    private final RoleBase role;
+    private final TeamList oldTeam;
+    private final TeamList newTeam;
+    private boolean cancel = false;
+
+    public TeamChangeEvent(RoleBase role, TeamList oldTeam, TeamList newTeam) {
+        this.role = role;
+        this.oldTeam = oldTeam;
+        this.newTeam = newTeam;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return this.cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancel = b;
+    }
+}
