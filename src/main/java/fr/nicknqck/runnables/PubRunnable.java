f7a83771602238f6ea9824cde701378244e5ea30
NickNqck <jadorevolvic@gmail.com>
2025-05-08 14:00:59 +0200

    Maj PUB runnable

--- src/main/java/fr/nicknqck/runnables/PubRunnable.java
+++ src/main/java/fr/nicknqck/runnables/PubRunnable.java
@@ -1,7 +1,6 @@
 package fr.nicknqck.runnables;
 
 import fr.nicknqck.Main;
-import fr.nicknqck.roles.desc.AllDesc;
 import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,16 +14,16 @@ public class PubRunnable extends BukkitRunnable {
     public void run() {
         if (time == random) {
             if (rank == 0) {
-                Bukkit.broadcastMessage(AllDesc.bar+"\n\n§bLa commande§6 /color <joueur1> <joueur2> ect§b est maintenant disponible§b.");
+                Bukkit.broadcastMessage("\n§bLa commande§6 /color <joueur1> <joueur2> ect§b est maintenant disponible§b.");
             }
             if (rank == 1) {
-                Bukkit.broadcastMessage(AllDesc.bar+"\n\n§bSi vous rencontrez des bugs, n'hésitez surtout pas à le notifier sur le serveur§6 /discord§b.");
+                Bukkit.broadcastMessage("\n§bSi vous rencontrez des bugs, n'hésitez surtout pas à le notifier sur le serveur§6 /discord§b.");
             }
             if (rank == 2) {
-                Bukkit.broadcastMessage(AllDesc.bar+"\n\n§bIl est maintenant possible de savoir ou vous visez avec votre ROLE ! Via la commande§6 /settings§b.");
+                Bukkit.broadcastMessage("\n§bIl est maintenant possible de savoir ou vous visez avec votre ROLE ! Via la commande§6 /settings§b.");
             }
             if (rank == 3){
-                Bukkit.broadcastMessage(AllDesc.bar+"\n\n§bUn SoundPack est disponible avec la commande§6 /pack§b.");
+                Bukkit.broadcastMessage("\n§bUn SoundPack est disponible avec la commande§6 /pack§b.");
             }
             rank++;
             random = Main.RANDOM.nextInt(60*10);
