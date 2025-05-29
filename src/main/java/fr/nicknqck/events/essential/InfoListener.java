9f0cb3e1449d12fbc5820d5cf8034ecd226ab201
NickNqck <jadorevolvic@gmail.com>
2025-05-29 19:06:18 +0200

    Modification du /info

--- src/main/java/fr/nicknqck/events/essential/InfoListener.java
+++ src/main/java/fr/nicknqck/events/essential/InfoListener.java
@@ -1,8 +1,9 @@
 package fr.nicknqck.events.essential;
 
 import fr.nicknqck.Main;
-import fr.nicknqck.events.custom.RoleGiveEvent;
+import fr.nicknqck.events.custom.*;
 import fr.nicknqck.events.custom.roles.TeamChangeEvent;
+import fr.nicknqck.events.custom.time.OnSecond;
 import fr.nicknqck.player.GamePlayer;
 import fr.nicknqck.player.PlayerInfo;
 import org.bukkit.entity.Arrow;
@@ -13,7 +14,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,12 +29,6 @@ public class InfoListener implements Listener {
         PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(uuid);
         info.incrementJoin();
         Main.getInstance().getInfoManager().save(uuid);
-    }
-    @EventHandler(priority = EventPriority.HIGHEST)
-    private void onDisconnect(final PlayerQuitEvent event) {
-        final PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(event.getPlayer().getUniqueId());
-        info.incrementQuit();
-        Main.getInstance().getInfoManager().save(event.getPlayer().getUniqueId());
     }
     @EventHandler(priority = EventPriority.HIGHEST)
     private void onShoot(final EntityShootBowEvent event) {
@@ -83,5 +77,55 @@ public class InfoListener implements Listener {
         info.addAmountTeamChange();
         info.addTeamPlayed(event.getNewTeam());
         Main.getInstance().getInfoManager().save(event.getRole().getPlayer());
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onGameEnd(final EndGameEvent event) {
+        if (event.getTeam() == null)return;
+        for (final GamePlayer gamePlayer : event.getGameState().getGamePlayer().values()) {
+            if (gamePlayer.getRole() == null)return;
+            final PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(gamePlayer.getUuid());
+            if (gamePlayer.getRole().getTeam().equals(event.getTeam())) {
+                info.addGameWin();
+            } else {
+                info.addGameLoose();
+            }
+            Main.getInstance().getInfoManager().save(gamePlayer.getUuid());
+        }
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onGameStart(final StartGameEvent event) {
+        System.out.println(0);
+        for (final UUID uuid : event.getInGamePlayers()) {
+            System.out.println(1);
+            final PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(uuid);
+            info.addGamePlayed();
+            Main.getInstance().getInfoManager().save(uuid);
+        }
+        System.out.println(2);
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onDeath(final UHCDeathEvent event) {
+        final PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(event.getPlayer().getUniqueId());
+        info.addDeath();
+        Main.getInstance().getInfoManager().save(event.getPlayer().getUniqueId());
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onKill(final UHCPlayerKillEvent event) {
+        if (event.getPlayerKiller() == null)return;
+        final PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(event.getPlayerKiller().getUniqueId());
+        info.addKill(event.getVictim().getUniqueId());
+        Main.getInstance().getInfoManager().save(event.getPlayerKiller().getUniqueId());
+    }
+    @EventHandler
+    private void onSecond(OnSecond onSecond) {
+        if (onSecond.isInGame()){
+            for (final GamePlayer gamePlayer : onSecond.getGameState().getGamePlayer().values()) {
+                if (!gamePlayer.isAlive())continue;
+                if (!gamePlayer.isOnline())continue;
+                final PlayerInfo info = Main.getInstance().getInfoManager().getPlayerInfo(gamePlayer.getUuid());
+                if (info == null)continue;
+                info.addTimePlayed();
+            }
+        }
     }
 }
--- src/main/java/fr/nicknqck/player/PlayerInfo.java
+++ src/main/java/fr/nicknqck/player/PlayerInfo.java
@@ -2,26 +2,50 @@ package fr.nicknqck.player;
 
 import fr.nicknqck.roles.builder.TeamList;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @Getter
 public class PlayerInfo {
 
     private final Map<String, Integer> rolesPlayed = new LinkedHashMap<>();
     private final Map<TeamList, Integer> teamPlayed = new LinkedHashMap<>();
+    private final Map<UUID, Integer> playerKills = new LinkedHashMap<>();
+    private final UUID uuid;
     private int joinCount = 0;
-    private int quitCount = 0;
     private int entitiesKilled = 0;
     private int arrowsShot = 0;
     private int amountTeamChange = 0;
+    private int gameWin = 0;
+    private int gameLoose = 0;
+    private int gamePlayed = 0;
+    private int totalKills = 0;
+    private int deaths = 0;
+    private int timePlayed = 0;
+
+    public PlayerInfo(UUID uuid) {
+        this.uuid = uuid;
+    }
 
     public void incrementJoin() { joinCount++; }
-    public void incrementQuit() { quitCount++; }
     public void incrementKills() { entitiesKilled++; }
     public void incrementArrows() { arrowsShot++; }
     public void addAmountTeamChange() { this.amountTeamChange++;}
+    public void addTimePlayed() {this.timePlayed++;}
+    public void addGameWin() {
+        this.gameWin++;
+    }
+    public void addGameLoose() {
+        this.gameLoose++;
+    }
+    public void addGamePlayed() {
+        System.out.println("Added gamePlayed");
+        this.gamePlayed++;
+    }
 
     public void incrementRolePlayed(String roleName) {
         rolesPlayed.put(roleName, rolesPlayed.getOrDefault(roleName, 0) + 1);
@@ -29,4 +53,29 @@ public class PlayerInfo {
     public void addTeamPlayed(final TeamList teamList) {
         this.teamPlayed.put(teamList, this.teamPlayed.getOrDefault(teamList, 0)+1);
     }
+    public void addKill(UUID victimUUID) {
+        playerKills.put(victimUUID, playerKills.getOrDefault(victimUUID, 0) + 1);
+        totalKills++;
+    }
+    public void addDeath() {
+        deaths++;
+    }
+    public String getMostKilledPlayerName() {
+        return playerKills.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .map(entry -> {
+                    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(entry.getKey());
+                    String name = (offlinePlayer != null && offlinePlayer.getName() != null) ? offlinePlayer.getName() : "Inconnu";
+                    int count = entry.getValue();
+                    double percentage = (totalKills == 0) ? 0 : (count * 100.0 / totalKills);
+                    return name + " (" + String.format("%.1f", percentage) + "%)";
+                })
+                .orElse("Aucun");
+    }
+
+    public String getKDRatio() {
+        if (deaths == 0) return totalKills + "/0";//Si on n'est jamais mort on return notre nombre de kill / 0
+        return totalKills +"/"+this.deaths+ " (" + String.format("%.2f", totalKills / (double) deaths) + ")";
+    }
+
 }
