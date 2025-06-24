1198b6a054b9d659d921de7a708e8d08fb66047a
NickNqck <jadorevolvic@gmail.com>
2025-06-24 18:12:38 +0200

    Maj desc event

--- src/main/java/fr/nicknqck/events/ns/EveilTenseiGan.java
+++ src/main/java/fr/nicknqck/events/ns/EveilTenseiGan.java
@@ -129,7 +129,7 @@ public class EveilTenseiGan extends Event implements Listener {
                 "§7sur le cadavre d'un autre porteur du§a Byakugan§7 (ou s'il n'y en a pas d'autre dans la partie),",
                 "§7il recevra les items§b Mode Chakra§7 et§a Sphère de Vérité§7.","",
                 "§8 -§b Mode Chakra§7: Possède une banque de temps de§c 5 minutes§7,",
-                "§7quand le pouvoir est activé l'utilisateur possède§b Speed II§7 et",
+                "§7quand le pouvoir est activé l'utilisateur possède§b Speed II§7,§a NoFall§7 et",
                 "§7à§c 5%§7 de§c chance§7 d'§6enflammer§7 le joueur attaquer.",
                 "",
                 "§8 -§b Sphere de vérité§7: Permet d'activer le fait de pouvoir§a voler§7,",
