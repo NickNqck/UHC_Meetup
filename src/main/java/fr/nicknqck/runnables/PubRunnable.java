adec2c1c39e48c2a98f48998238be411f29bb677
NickNqck <jadorevolvic@gmail.com>
2025-09-08 12:29:02 +0200

    Fix pub

--- src/main/java/fr/nicknqck/runnables/PubRunnable.java
+++ src/main/java/fr/nicknqck/runnables/PubRunnable.java
@@ -13,23 +13,27 @@ public class PubRunnable extends BukkitRunnable {
     @Override
     public void run() {
         if (time == random) {
+            random = Main.RANDOM.nextInt(60*10);
             if (rank == 0) {
                 Bukkit.broadcastMessage("\n§bLa commande§6 /color <joueur1> <joueur2> ect§b est maintenant disponible§b.");
                 changeRank(0);
+                return;
             }
             if (rank == 1) {
                 Bukkit.broadcastMessage("\n§bSi vous rencontrez des bugs, n'hésitez surtout pas à le notifier sur le serveur§6 /discord§b.");
                 changeRank(1);
+                return;
             }
             if (rank == 2) {
                 Bukkit.broadcastMessage("\n§bIl est maintenant possible de savoir ou vous visez avec votre ROLE ! Via la commande§6 /settings§b.");
                 changeRank(2);
+                return;
             }
             if (rank >= 3){
                 Bukkit.broadcastMessage("\n§bUn SoundPack est disponible avec la commande§6 /pack§b.");
                 changeRank(3);
+                return;
             }
-            random = Main.RANDOM.nextInt(60*10);
             return;
         }
         this.time++;
