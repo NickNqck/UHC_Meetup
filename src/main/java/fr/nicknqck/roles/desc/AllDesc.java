02b5c0b409f6c306f232252d03397182fd0768bb
NickNqck <jadorevolvic@gmail.com>
2026-07-06 09:54:05 +0200

    Suppression de l'ancien code de Hantengu

--- src/main/java/fr/nicknqck/roles/desc/AllDesc.java
+++ src/main/java/fr/nicknqck/roles/desc/AllDesc.java
@@ -56,32 +56,7 @@ public class AllDesc{
             "",
             bar	
 	};
-	public static String[] HantenguV2 = new String[] {
-			bar,
-			"§lRôle: §r§cHantenguV2",
-			"",
-			ChatColor.BOLD+"Effet: ",
-			"",
-			(ChatColor.DARK_GRAY+" • " +ChatColor.WHITE+"(Si vous êtes Hantengu)§b "+Speed+" 2§r, Invisibilité 1 et§a No Fall§r en enlevant son armure, "+weak+" 1 permanent"),
-			"",
-			ChatColor.BOLD+"Items: ",
-			"",
-			(ChatColor.DARK_GRAY+" • " +ChatColor.GOLD+ChatColor.BOLD+"Materialisation des émotions: "+ChatColor.WHITE+"Ouvre un menu vous offrant la possibilité de choisir entre plusieurs clone de vous via un arbre de compétence, la première fois vous aurez le choix entre§c Karaku§r et§c Sekido§r puis entre§6Urogi§r et§6 Urami§r (si vous avez choisis§c Karaku§r) sinon entre§6 Aizetsu§r et§6 Urami§r (si vous avez choisis§c Sekido§r), puis à la toute fin vous aurez quoi qu'il arrive accès à§6 Zohakuten"),
-			"",
-			ChatColor.RED+"Karaku§r: Vous donne "+Speed+" 1 permanent ainsi que l'item§6 Vent§r qui vous permettra de tp les joueurs autours de vous§6 50blocs§r en hauteur",
-			"",
-			ChatColor.RED+"Sekido§r: Vous donne "+Force+" 1 permanent ainsi que l'item§6 Kakkhara§r qui via un clique droit créera un§e éclair§r sur tout les joueurs non-démon étant à moins de§6 25blocs§r qui leur infligera 2"+coeur+" ainsi que "+blind+" 1 et§9 "+slow+" 4§r pendant§l 4s",
-			"",
-			ChatColor.GOLD+"Urogi§r: Vous donne "+Speed+" 1 et "+Force+" 1 permanent ainsi que l'item§6 Urogi§r qui vous permettra de gagner l'effet§2 Jump Boost 4§r ainsi que 2 Double Jump qui vous propulseront d'environ 20blocs (Cooldown double jump: 30s), vous recevrez également l'item§6 Cri Sonique§r la personne visée obtiendra "+slow+" 1,§a Nausée 1§r et "+weak+" 1 pendant§6 30s",
-			"",
-			ChatColor.GOLD+"Urami§r: Vous donne "+Resi+" 2 ainsi que "+weak+" 1 permanent, via ce clone si vous mourrez avec vous réssuciter en temp qu'Hantengu mais avec 3"+coeur+" permanent en moins ",
-			"",
-			ChatColor.GOLD+"Aizetsu§r: Vous donne "+Force+" 1 permanent ainsi que 2"+coeur+" supplémentaire, vous obtiendrez via ce clone une épée nommé§6 Yari§r qui vous permettra via un clique droit de ne plus prendre de dégat pendant§6 5s",
-			"",
-			ChatColor.GOLD+"§oZohakuten§r: Vous donne "+Force+" 1 et "+Resi+" 1§r permanent ainsi qu'une "+regen+" naturelle de 0,5"+coeur+" toute les§6 15s§r de plus vous obtiendrez différent item en fonction des clones que vous avec choisis précédemment",
-			"",
-			bar
-	};
+
 	public static String[] JigoroV2Pacte1 = new String[] {
 			bar,
 			"§lRôle: §r§6Jigoro",
