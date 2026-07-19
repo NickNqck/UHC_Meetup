e89ed64d88b13a4f7b212fed5407af34a77faadb
NickNqck <jadorevolvic@gmail.com>
2026-07-19 12:04:23 +0200

    Ajout d'une pub pour le /inv

--- src/main/java/fr/nicknqck/Main.java
+++ src/main/java/fr/nicknqck/Main.java
@@ -177,6 +177,7 @@ public class Main extends JavaPlugin {
 		this.pubManager.add("§bSi vous rencontrez des bugs, n'hésitez pas à le notifier sur§6 /discord§b.");
 		this.pubManager.add("§bSachez où vous visez avec votre rôle via la commande§6 /settings§b.");
 		this.pubManager.add("§bUn SoundPack est disponible avec la commande§6 /pack§b.");
+		this.pubManager.add("§bIl est maintenant possible de customiser l'inventaire de départ par défaut via la commande§6 /inv§b.");
 		this.pubManager.start();
 		this.schematicManager = new SchematicManager(this);
 		this.crystalManager = new CrystalManager();
